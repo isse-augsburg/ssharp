5f15a2972ed6fa1f3de62448eac323b99fc0befc
Johannes Leupolz <johannes.leupolz@googlemail.com>
2015-11-27 15:48:13 +0100

    Transformation from CTL Formulas to LTSmin Mu-calculus Formulas

--- Source/SafetySharp/Analysis/FormulaVisitors/LtsMinMuCalculusTransformer.cs
+++ Source/SafetySharp/Analysis/FormulaVisitors/LtsMinMuCalculusTransformer.cs
@@ -22,6 +22,8 @@
 
 namespace SafetySharp.Analysis.FormulaVisitors
 {
+	using System;
+	using System.Diagnostics;
 	using System.Text;
 	using Utilities;
 
@@ -30,14 +32,27 @@ namespace SafetySharp.Analysis.FormulaVisitors
 	/// </summary>
 	/// <remarks>
 	///   LtsMin μ-calculus syntax: http://fmt.cs.utwente.nl/tools/ltsmin/doc/ltsmin-mucalc.html
+	///   Transformation from CTL to mu calculus from: E. Allen Emerson, Model Checking and the Mu-calculus
+	///   which is also a good introduction to the mu calculus and Model Checking itself.
 	/// </remarks>
 	internal class LtsMinMuCalculusTransformer : FormulaVisitor
 	{
+		// We support only CTL for now.
+
 		/// <summary>
 		///   The string builder that is used to construct the transformed formula.
 		/// </summary>
 		private readonly StringBuilder _builder = new StringBuilder();
 
+		private int _sigmaVariableCounter = 0;
+
+		private string CreateFreshSigmaVariableName()
+		{
+			// This function should return a fresh variable name, which does not occur in any StateFormula.
+			_sigmaVariableCounter += 1;
+			return $"sigma{_sigmaVariableCounter}";
+		}
+		
 		/// <summary>
 		///   Gets the transformed μ-calculus formula.
 		/// </summary>
@@ -49,21 +64,22 @@ namespace SafetySharp.Analysis.FormulaVisitors
 		public override void VisitUnaryFormula(UnaryFormula formula)
 		{
 			_builder.Append("(");
-
-			// TODO
+			
 			switch (formula.Operator)
 			{
 				case UnaryOperator.Next:
-					_builder.Append(" X ");
-					break;
 				case UnaryOperator.Finally:
-					_builder.Append(" <> ");
-					break;
 				case UnaryOperator.Globally:
-					_builder.Append(" [] ");
+					Assert.NotReached($"Path Operator only allowed in Path Formulas.");
 					break;
 				case UnaryOperator.Not:
 					_builder.Append(" ! ");
+					break;
+				case UnaryOperator.All:
+					VisitForAllPathFormula(formula.Operand);
+					break;
+				case UnaryOperator.Exists:
+					VisitExistsPathFormula(formula.Operand);
 					break;
 				default:
 					Assert.NotReached($"Unknown or unsupported unary operator '{formula.Operator}'.");
@@ -74,15 +90,145 @@ namespace SafetySharp.Analysis.FormulaVisitors
 			_builder.Append(")");
 		}
 
+		
+		public void VisitForAllPathFormula(Formula formula)
+		{
+			var freshSigmaVariable = "";
+			if (formula is BinaryFormula)
+			{
+				var binaryFormula = (BinaryFormula)formula;
+				switch (binaryFormula.Operator)
+				{
+					case BinaryOperator.And:
+					case BinaryOperator.Or:
+					case BinaryOperator.Implication:
+					case BinaryOperator.Equivalence:
+						Assert.NotReached($"Assumed to get a Path Operator.");
+						break;
+					case BinaryOperator.Until:
+						freshSigmaVariable = CreateFreshSigmaVariableName();
+						_builder.Append($"( mu {freshSigmaVariable}. (");
+						Visit(binaryFormula.RightOperand);
+						_builder.Append($" || (");
+						Visit(binaryFormula.LeftOperand);
+						_builder.Append($" && [] {freshSigmaVariable} )))");
+						break;
+					default:
+						Assert.NotReached($"Unknown or unsupported binary operator '{binaryFormula.Operator}'.");
+						break;
+				}
+			}
+			else if (formula is UnaryFormula)
+			{
+				var unaryFormula = (UnaryFormula)formula;
+				switch (unaryFormula.Operator)
+				{
+					case UnaryOperator.Next:
+						_builder.Append($" [] (");
+						Visit(unaryFormula.Operand);
+						_builder.Append($" )");
+						break;
+					case UnaryOperator.Finally:
+						freshSigmaVariable = CreateFreshSigmaVariableName();
+						_builder.Append($"( mu {freshSigmaVariable}. (");
+						Visit(unaryFormula.Operand);
+						_builder.Append($" || [] {freshSigmaVariable} ))");
+						break;
+					case UnaryOperator.Globally:
+						freshSigmaVariable = CreateFreshSigmaVariableName();
+						_builder.Append($"( nu {freshSigmaVariable}. (");
+						Visit(unaryFormula.Operand);
+						_builder.Append($" && [] {freshSigmaVariable} ))");
+						Visit(unaryFormula.Operand);
+						break;
+					case UnaryOperator.Not:
+						Assert.NotReached($"Assumed to get a Path Operator.");
+						break;
+					default:
+						Assert.NotReached($"Unknown or unsupported unary operator '{unaryFormula.Operator}'.");
+						break;
+				}
+			}
+			else
+			{
+				Assert.NotReached("Formula has to be either a BinaryFormula or an UnaryFormula");
+			}
+		}
+
+		public void VisitExistsPathFormula(Formula formula)
+		{
+			var freshSigmaVariable = "";
+			if (formula is BinaryFormula)
+			{
+				var binaryFormula = (BinaryFormula)formula;
+				switch (binaryFormula.Operator)
+				{
+					case BinaryOperator.And:
+					case BinaryOperator.Or:
+					case BinaryOperator.Implication:
+					case BinaryOperator.Equivalence:
+						Assert.NotReached($"Assumed to get a Path Operator.");
+						break;
+					case BinaryOperator.Until:
+						freshSigmaVariable = CreateFreshSigmaVariableName();
+						_builder.Append($"( mu {freshSigmaVariable}. (");
+						Visit(binaryFormula.RightOperand);
+						_builder.Append($" || (");
+						Visit(binaryFormula.LeftOperand);
+						_builder.Append($" && <> {freshSigmaVariable} )))");
+						break;
+					default:
+						Assert.NotReached($"Unknown or unsupported binary operator '{binaryFormula.Operator}'.");
+						break;
+				}
+			}
+			else if (formula is UnaryFormula)
+			{
+				var unaryFormula = (UnaryFormula)formula;
+				switch (unaryFormula.Operator)
+				{
+					case UnaryOperator.Next:
+						_builder.Append($" <> (");
+						Visit(unaryFormula.Operand);
+						_builder.Append($" )");
+						break;
+					case UnaryOperator.Finally:
+						freshSigmaVariable = CreateFreshSigmaVariableName();
+						_builder.Append($"( mu {freshSigmaVariable}. (");
+						Visit(unaryFormula.Operand);
+						_builder.Append($" || <> {freshSigmaVariable} ))");
+						break;
+					case UnaryOperator.Globally:
+						freshSigmaVariable = CreateFreshSigmaVariableName();
+						_builder.Append($"( nu {freshSigmaVariable}. (");
+						Visit(unaryFormula.Operand);
+						_builder.Append($" && <> {freshSigmaVariable} ))");
+						Visit(unaryFormula.Operand);
+						break;
+					case UnaryOperator.Not:
+						Assert.NotReached($"Assumed to get a Path Operator.");
+						break;
+					default:
+						Assert.NotReached($"Unknown or unsupported unary operator '{unaryFormula.Operator}'.");
+						break;
+				}
+			}
+			else
+			{
+				Assert.NotReached("Formula has to be either a BinaryFormula or an UnaryFormula");
+			}
+		}
+
+
 		/// <summary>
-		///   Visits the <paramref name="formula" />.
+		///   Visits the <paramref name="formula"/>. This expects a CTL State Formula.
 		/// </summary>
 		public override void VisitBinaryFormula(BinaryFormula formula)
 		{
+			// We assume that the topMost operator is either A or G
 			_builder.Append("(");
 			Visit(formula.LeftOperand);
-
-			// TODO
+			
 			switch (formula.Operator)
 			{
 				case BinaryOperator.And:
@@ -98,7 +244,7 @@ namespace SafetySharp.Analysis.FormulaVisitors
 					_builder.Append(" <-> ");
 					break;
 				case BinaryOperator.Until:
-					_builder.Append(" U ");
+					Assert.NotReached($"Until only allowed in Path Formulas.");
 					break;
 				default:
 					Assert.NotReached($"Unknown or unsupported binary operator '{formula.Operator}'.");
