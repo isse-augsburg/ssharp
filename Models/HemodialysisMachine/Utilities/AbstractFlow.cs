2e0c11a5a651d745d9b2699f545e31261f7670d5
Johannes Leupolz <johannes.leupolz@googlemail.com>
2015-12-17 17:55:34 +0100

    AbstractFlow improved

--- Models/HemodialysisMachine/Utilities/AbstractFlow.cs
+++ Models/HemodialysisMachine/Utilities/AbstractFlow.cs
@@ -4,213 +4,349 @@ namespace HemodialysisMachine.Utilities
 {
 	using System.Linq;
 
-	// - The updates must be called in topological sorted order starting at the source.
-	// - Incoming Values of an element must be injected by setting FlowElementBefore
-	// - Source elements use their Lambda-Function to generate the outgoing value and setting ValueOfCurrentCylce
-	// - Flow elements get their incomingValue through a connection (which they retrieve by calling FlowElementBefore)
-	// - Flow elements set their outgoingValue by setting ValueOfCurrentCylce using their lambda function.
-	// - Sink elements get their incomingValue through a connection (which they retrieve by calling FlowElementBefore)
+
+	// Flow
+	//    - Every flow is orchestrated by a Flow class.
+	//    - Flows consist of FlowComponents (FlowComposites, FlowSegments, FlowSources, and FlowSinks)
+	//    - Refreshes every Cycle
+	//    - Every FlowComponent might have outgoing ports (IFlowOut) or incoming ports (IFlowIn)
+	//    - First, every UpdateSuction is executed (from Sink to Source)
+	//    - Then, every UpdateElement is executed (from Source to Sink)
+	//    - Must be acyclic
+	// Port
+	//    - Consists of two parts (Suction and PushElement)
+	//    - Every time the _active_ part should call its part
+	//    - For Suction the active part is the respective latter component of the flow
+	//    - For PushElement the active part is the respective former component of the flow
+	// FlowScheduler
+	//    - TODO: could be created by FlowConnector
 	// Example:
 	//   We have
 	//      - a Source source
 	//      - a DirectFlow directFlow
 	//      - a Sink sink
 	//   They are connected
-	//      - directFlow.FlowElementBefore = source.FlowElementAfterwards (ConnectOutWithIn)
-	//      - sink.FlowElementBefore = directFlow.FlowElementAfterwards (ConnectOutWithIn)
-	//      - directFlow.FlowElementAfterwards = directFlow.FlowElementBefore (Behavior of DirectFlow)
+	//      - directFlow.ElementBefore = source.ElementAfterwards (ConnectOutWithIn)
+	//      - sink.ElementBefore = directFlow.ElementAfterwards (ConnectOutWithIn)
+	//      - directFlow.ElementAfterwards = directFlow.ElementBefore (Behavior of DirectFlow)
 	//   Scenario
 	//		- source.Update () is called
 	//			- source.SourceLambdaFunc is called
-	//			- source.ValueOfCurrentCylce is set
+	//			- source.ElementOfCurrentCycle is set
 	//		- directFlow.Update () is called
-	//			- directFlow.FlowElementBefore() is called
-	//			- source.FlowElementAfterwards() is called
+	//			- directFlow.ElementBefore(requestedSuction) is called
+	//			- source.ElementAfterwards() is called and requestedSuction for the next step is saved in source
 	//			- directFlow.FlowLambdaFunc() is called
-	//			- directFlow.ValueOfCurrentCylce is set
+	//			- directFlow.ElementOfCurrentCycle is set
 	//		- sink.Update () is called
-	//			- sink.FlowElementBefore() is called
+	//			- sink.ElementBefore(requestedSuction) is called
+	//			- directFlow.ElementAfterwards() is called and requestedSuction for the next step is saved in directFlow
 	//			//- sink.SinkLambdaFunc is called
-	//			- sink.ValueOfCurrentCylce is set
+	//			- sink.ElementOfCurrentCycle is set
+
+	// - The direction of the flow _never_ changes. Only the suction
+	// - TElement contains the quantity of the element 
+	// - ElementBefore(uint suction) contains a suction parameter which valid for the _next_ step
+	//   (only pressure difference). A source may have an own output pressure. These values are added.
+	// -  Higher pressure of the surrounding leads to suction effect. This suction is propagated to the source
+	//    and in the next step this has an effect.
+	// - Problem: The requestedSuction needs several ticks to propagate from the sink to the source
+
+	// Comments and further ideas:
+	//   - Modelica solves our problems by equations.
 
 
-	// Comment: Modelica solves our problems by equations.
+	public delegate void SetElementDelegate<TElement>(TElement pushedElement);
+	public delegate void SetSuctionDelegate(int suction);
 
-	public interface IFlowIn<TElement>
+
+
+	public class PortFlowIn<TElement> where TElement : struct
 	{
-		Func<TElement> FlowElementBefore { get; set; }
+		public SetElementDelegate<TElement> SetElement { get; set; }
+		public SetSuctionDelegate SetPredecessorSuction { get; set; }
 	}
 
-	public interface IFlowOut<TElement>
+	public class PortFlowOut<TElement> where TElement : struct
 	{
-		TElement FlowElementAfterwards();
+		public SetElementDelegate<TElement> SetSuccessorElement { get; set; }
+		public SetSuctionDelegate SetSuction { get; set; }
+	}
+
+	public interface IFlowIn<TElement> where TElement : struct
+	{
+		PortFlowIn<TElement> Incoming { get; set; }
+	}
+
+	public interface IFlowOut<TElement> where TElement : struct
+	{
+		PortFlowOut<TElement> Outgoing { get; set; }
+	}
+
+
+	public class PortConnector<TElement> where TElement : struct
+	{
+		public static void Connect(PortFlowIn<TElement> fromOuter, PortFlowIn<TElement> toInner)
+		{
+			// ConnectInWithIn
+			// Mainly used for CompositeFlows
+			// When fromOuter.SetElement is called, then toInner.SetElement should be called.
+			// When toInner.SetPredecessorSuction is called, then fromOuter.SetPredecessorSuction should be called.
+			if (toInner.SetPredecessorSuction != null)
+				throw new Exception("toInner is already connected");
+			fromOuter.SetElement = element =>  toInner.SetElement(element);
+			toInner.SetPredecessorSuction = suction => fromOuter.SetPredecessorSuction(suction);
+		}
+
+		public static void Connect(PortFlowOut<TElement> @from, PortFlowIn<TElement> to)
+		{
+			// ConnectOutWithIn
+			// Flow goes from [@from]-->[to]
+			// Suction goes from [to]-->[@from]
+			// When from.SetSuccessorElement is called, then to.SetElement should be called.
+			// When to.SetPredecessorSuction is called, then from.SetSuction should be called.
+			if (from.SetSuccessorElement != null)
+				throw new Exception("is already connected");
+			from.SetSuccessorElement = element => to.SetElement(element);
+			to.SetPredecessorSuction = suction => from.SetSuction(suction);
+		}
+
+		public static void Connect(PortFlowOut<TElement> fromInner, PortFlowOut<TElement> toOuter)
+		{
+			// ConnectOutWithOut
+			// Mainly used for CompositeFlows
+			// When fromInner.SetSuccessorElement is called, then toOuter.SetSuccessorElement should be called.
+			// When toOuter.SetSuction is called, then fromInner.SetSuction should be called.
+			if (toOuter.SetSuccessorElement != null)
+				throw new Exception("toOuter is already connected");
+			fromInner.SetSuccessorElement = element => toOuter.SetSuccessorElement(element);
+			toOuter.SetSuction = suction => fromInner.SetSuction(suction);
+		}
+	}
+	
+	public abstract class FlowConnector<TElement> where TElement : struct
+	{
+		public void ConnectInWithIn(FlowComposite<TElement> @from, IFlowIn<TElement> to)
+		{
+			PortConnector<TElement>.Connect(@from.Incoming,to.Incoming);
+		}
+
+		public void ConnectOutWithIn(IFlowOut<TElement> @from, IFlowIn<TElement> to)
+		{
+			PortConnector<TElement>.Connect(@from.Outgoing, to.Incoming);
+		}
+
+		public void ConnectOutWithOut(IFlowOut<TElement> @from, FlowComposite<TElement> to)
+		{
+			PortConnector<TElement>.Connect(@from.Outgoing, to.Outgoing);
+		}
+	}
+
+	public class FlowSegment<TElement> : IFlowIn<TElement>, IFlowOut<TElement> where TElement : struct
+	{
+		public PortFlowIn<TElement> Incoming { get; set; }
+		public PortFlowOut<TElement> Outgoing { get; set; }
+
+		public TElement ElementOfCurrentCycle { get; set; }
+		public int SuctionOfCurrentCycle { get; set; }
+		private Func<TElement, TElement> FlowLambdaFunc { get; set; }
+		
+		public FlowSegment(Func<TElement, TElement> flowLambdaFunc)
+		{
+			FlowLambdaFunc = flowLambdaFunc;
+			Incoming = new PortFlowIn<TElement>();
+			Incoming.SetElement = value => ElementOfCurrentCycle = value;
+			Outgoing = new PortFlowOut<TElement>();
+			Outgoing.SetSuction = value => SuctionOfCurrentCycle = value;
+		}
+
+		public void UpdateSuction()
+		{
+			// Actively Update the SuctionOfCurrentCycle of the predecessor
+			Incoming.SetPredecessorSuction(SuctionOfCurrentCycle);
+		}
+
+		public void UpdateElement()
+		{
+			// Actively Update the ElementOfCurrentCycle of the successor
+			ElementOfCurrentCycle = FlowLambdaFunc(ElementOfCurrentCycle);
+			Outgoing.SetSuccessorElement(ElementOfCurrentCycle);
+		}
+	}
+
+	public class FlowSource<TElement> : IFlowOut<TElement>  where TElement : struct
+	{
+		public PortFlowOut<TElement> Outgoing { get; set; }
+
+		private Func<TElement> SourceLambdaFunc { get; }
+		public TElement ElementOfCurrentCycle { get; set; }
+		public int SuctionOfCurrentCycle { get; set; }
+
+		public FlowSource(Func<TElement> sourceLambdaFunc)
+		{
+			SourceLambdaFunc = sourceLambdaFunc;
+			Outgoing = new PortFlowOut<TElement>();
+			Outgoing.SetSuction = value => SuctionOfCurrentCycle = value;
+		}
+		
+		public void UpdateElement()
+		{
+			// Actively Update the ElementOfCurrentCycle of the successor
+			ElementOfCurrentCycle = SourceLambdaFunc();
+			Outgoing.SetSuccessorElement(ElementOfCurrentCycle);
+		}
+	}
+
+	public class FlowSink<TElement> : IFlowIn<TElement> where TElement : struct
+	{
+		public PortFlowIn<TElement> Incoming { get; set; }
+
+		public int SuctionOfCurrentCycle { get; set; }
+
+		public FlowSink(int initialSuction)
+		{
+			SuctionOfCurrentCycle = initialSuction;
+			Incoming = new PortFlowIn<TElement>();
+			Incoming.SetElement = value => ElementOfCurrentCycle = value;
+		}
+
+		public FlowSink()
+			: this(1) //default is a sink that has a suction of 1 (to let at least anything flow)
+		{
+		}
+
+		protected TElement ElementOfCurrentCycle { get; private set; }
+		
+		public void UpdateSuction()
+		{
+			// Actively Update the SuctionOfCurrentCycle of the predecessor
+			Incoming.SetPredecessorSuction(SuctionOfCurrentCycle);
+		}
+	}
+
+	public class FlowComposite<TElement> : IFlowIn<TElement>, IFlowOut<TElement> where TElement : struct
+	{
+		public PortFlowIn<TElement> Incoming { get; set; }
+		public PortFlowOut<TElement> Outgoing { get; set; }
+
+		//public int SuctionOfCurrentCycle { get; set; } TODO: Create proxy port, if value is of interest
+		//public TElement ElementOfCurrentCycle { get; set; } TODO: Create proxy port, if value is of interest
+
+		public FlowComposite()
+		{
+			Incoming = new PortFlowIn<TElement>();
+			Outgoing = new PortFlowOut<TElement>();
+		}
+	}
+	
+	public class FlowVirtualSplitter<TElement> : IFlowIn<TElement> where TElement : struct
+	{
+		private int Number { get; }
+		public PortFlowIn<TElement> Incoming { get; set; }
+		public PortFlowOut<TElement>[] Outgoings { get; set; }
+
+		// Elements must be split
+		public TElement ElementOfCurrentCycle { get; set; }
+		public TElement[] ElementOfCurrentCycles { get; set; }
+
+		// Suctions must be merged
+		public int[] SuctionOfCurrentCycles { get; set; }
+		public int SuctionOfCurrentCycle { get; set; }
+
+		public FlowVirtualSplitter(int number)
+		{
+			Number = number;
+			Incoming.SetElement = element => ElementOfCurrentCycle = element;
+			Incoming = new PortFlowIn<TElement>();
+			Outgoings = new PortFlowOut<TElement>[number];
+			ElementOfCurrentCycles = new TElement[number];
+			for (int i = 0; i < number; i++)
+			{
+				Outgoings[i] = new PortFlowOut<TElement>();
+				Outgoings[i].SetSuction = (value) => SuctionOfCurrentCycles[i] = value;
+			}
+		}
+
+		public void UpdateSuction()
+		{
+			// TODO: Update SuctionOfCurrentCycle
+			// Actively Update the SuctionOfCurrentCycle of the predecessor
+			Incoming.SetPredecessorSuction(SuctionOfCurrentCycle);
+		}
+
+		public void UpdateElement()
+		{
+			// TODO: Update ElementOfCurrentCycles[i]
+			// Actively Update the ElementOfCurrentCycle of the successors
+			for (int i = 0; i < Number; i++)
+			{
+				Outgoings[i].SetSuccessorElement(ElementOfCurrentCycles[i]);
+			}
+		}
+	}
+
+	public class FlowVirtualMerger<TElement> : IFlowOut<TElement> where TElement : struct
+	{
+		private int Number { get; }
+		public PortFlowIn<TElement>[] Incomings { get; set; }
+		public PortFlowOut<TElement> Outgoing { get; set; }
+
+		// Elements must be merged
+		public TElement[] ElementOfCurrentCycles { get; set; }
+		public TElement ElementOfCurrentCycle { get; set; }
+
+		// Suctions must be split
+		public int SuctionOfCurrentCycle { get; set; }
+		public int[] SuctionOfCurrentCycles { get; set; }
+
+		public FlowVirtualMerger(int number)
+		{
+			Number = number;
+			Outgoing.SetSuction = suction => SuctionOfCurrentCycle = suction;
+			Incomings = new PortFlowIn<TElement>[number];
+			Outgoing = new PortFlowOut<TElement>();
+			SuctionOfCurrentCycles = new int[number];
+			for (int i = 0; i < number; i++)
+			{
+				Incomings[i] = new PortFlowIn<TElement>();
+				Incomings[i].SetElement = (element) => ElementOfCurrentCycles[i] = element;
+			}
+		}
+
+		public void UpdateSuction()
+		{
+			// TODO: Update SuctionOfCurrentCycles[i]
+			// Actively Update the SuctionOfCurrentCycle of the predecessors
+			for (int i = 0; i < Number; i++)
+			{
+				Incomings[i].SetPredecessorSuction(SuctionOfCurrentCycles[i]);
+			}
+		}
+
+		public void UpdateElement()
+		{
+			// TODO: Update ElementOfCurrentCycle
+			// Actively Update the ElementOfCurrentCycle of the successor
+			Outgoing.SetSuccessorElement(ElementOfCurrentCycle);
+		}
 	}
 
 	public static class FlowConnectors
 	{
-		public static TElement[] SplitEqual<TElement>(TElement source, int splitsTotal)
+		public static TElement[] SplitEqual<TElement>(TElement source, int splitsTotal) where TElement : struct
 		{
 			return System.Linq.Enumerable.Repeat<TElement>(source, splitsTotal).ToArray();
 		}
 
-		public static TElement MergeAny<TElement>(TElement[] sources)
+		public static TElement MergeAny<TElement>(TElement[] sources) where TElement : struct
 		{
 			return sources[0];
 		}
 	}
 
-	public abstract class FlowConnector<TElement>
+	public class FlowDirect<TElement> : FlowSegment<TElement> where TElement : struct
 	{
-		protected abstract TElement Merger(TElement[] sources);
-
-		protected abstract TElement[] Splitter(TElement source,int splits);
-
-		public void ConnectInWithIn(CompositeFlow<TElement> @from, IFlowIn<TElement> to)
-		{
-			// Only works for CompositeFlows
-			if (to!=null)
-				throw new Exception("to is already connected");
-			to.FlowElementBefore = from.FlowElementBefore;
-		}
-
-
-		public void ConnectInWithIn(CompositeFlow<TElement> @from, params IFlowIn<TElement>[] to)
-		{
-			if (to != null)
-				throw new Exception("to is already connected");
-			var fromInValue = @from.FlowElementBefore();
-			var fromInValues = Splitter(fromInValue, to.Length);
-			for (int i = 0; i < to.Length; i++)
-			{
-				to[i].FlowElementBefore = () => fromInValues[i];
-			}
-		}
-
-		public void ConnectOutWithIn(IFlowOut<TElement> @from, IFlowIn<TElement> to)
-		{
-			// The FlowElement the from-component returns is the FlowElement the to-component receives.
-			if (to != null)
-				throw new Exception("to is already connected");
-			to.FlowElementBefore = from.FlowElementAfterwards;
-		}
-
-		public void ConnectOutWithIn(IFlowOut<TElement>[] fromOuts, IFlowIn<TElement> to)
-		{
-			if (to != null)
-				throw new Exception("to is already connected");
-			var fromOutValues = fromOuts.Select(fromOut => fromOut.FlowElementAfterwards());
-			to.FlowElementBefore = () => Merger(fromOutValues.ToArray());
-		}
-
-		public void ConnectOutWithIn(IFlowOut<TElement> @from, params IFlowIn<TElement>[] to)
-		{
-			if (to != null)
-				throw new Exception("to is already connected");
-			var fromOutValue = @from.FlowElementAfterwards();
-			var toInValues = Splitter(fromOutValue, to.Length);
-			for (int i = 0; i < to.Length; i++)
-			{
-				to[i].FlowElementBefore = () => toInValues[i];
-			}
-		}
-
-		public void ConnectOutWithOut(IFlowOut<TElement> @from, CompositeFlow<TElement> to)
-		{
-			if (to != null)
-				throw new Exception("to is already connected");
-			to.LastOutputOfComposition = from.FlowElementAfterwards;
-		}
-
-		public void ConnectOutWithOut(IFlowOut<TElement>[] fromOuts, CompositeFlow<TElement> to)
-		{
-			if (to != null)
-				throw new Exception("to is already connected");
-			var fromOutValues = fromOuts.Select(fromOut => fromOut.FlowElementAfterwards());
-			to.LastOutputOfComposition = () => Merger(fromOutValues.ToArray());
-		}
-	}
-
-	public class Flow<TElement> : IFlowIn<TElement>, IFlowOut<TElement>
-	{
-		public Func<TElement> FlowElementBefore { get; set; }
-
-		private Func<TElement,TElement> FlowLambdaFunc { get; }
-		protected TElement ValueOfCurrentCylce { get; private set; }
-
-		public TElement FlowElementAfterwards()
-		{
-			return ValueOfCurrentCylce;
-		}
-
-		public Flow(Func<TElement, TElement> flowLambdaFunc)
-		{
-			FlowLambdaFunc = flowLambdaFunc;
-		}
-
-		public void Update()
-		{
-			var incomingValue = FlowElementBefore();
-			ValueOfCurrentCylce = FlowLambdaFunc(incomingValue);
-		}
-	}
-
-	public class FlowSource<TElement> : IFlowOut<TElement>
-	{
-		private Func<TElement> SourceLambdaFunc { get; }
-		protected TElement ValueOfCurrentCylce { get; private set; }
-		
-		public TElement FlowElementAfterwards()
-		{
-			return ValueOfCurrentCylce;
-		}
-
-		public FlowSource(Func<TElement> sourceLambdaFunc)
-		{
-			SourceLambdaFunc = sourceLambdaFunc;
-		}
-
-		public void Update()
-		{
-			ValueOfCurrentCylce = SourceLambdaFunc();
-		}
-	}
-
-	public class FlowSink<TElement> : IFlowIn<TElement>
-	{
-		public Func<TElement> FlowElementBefore { get; set; }
-		
-		protected TElement ValueOfCurrentCylce { get; private set; }
-		
-		public void Update()
-		{
-			var incomingValue = FlowElementBefore();
-			ValueOfCurrentCylce = incomingValue;
-		}
-	}
-
-	public class CompositeFlow<TElement> : IFlowIn<TElement>, IFlowOut<TElement>
-	{
-		// flow declared explicitly by modeler using connections
-
-		public Func<TElement> FlowElementBefore { get; set; }
-		public Func<TElement> LastOutputOfComposition { get; set; }
-
-		public TElement ValueOfCurrentCylce { get; set; }
-		
-		public TElement FlowElementAfterwards()
-		{
-			return ValueOfCurrentCylce;
-		}
-
-		public void Update()
-		{
-			ValueOfCurrentCylce = LastOutputOfComposition();
-		}
-	}
-
-	public class DirectFlow<TElement> : Flow<TElement>, IFlowIn<TElement>, IFlowOut<TElement>
-	{
-		public DirectFlow()
+		public FlowDirect()
 			: base( inValue => inValue)
 		{
 		}
