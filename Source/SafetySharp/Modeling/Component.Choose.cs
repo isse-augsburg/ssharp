7b18530d1b6c66cbe544a5e13dec4dfe0bdd3414
Johannes Leupolz <johannes.leupolz@googlemail.com>
2016-04-11 12:04:35 +0200

    Probability: New tests

--- Source/SafetySharp/Modeling/Component.Choose.cs
+++ Source/SafetySharp/Modeling/Component.Choose.cs
@@ -158,5 +158,101 @@ namespace SafetySharp.Modeling
 		{
 			return _defaultChoice.Choose(values);
 		}
+		
+
+		/// <summary>
+		///   Deterministically returns the <paramref name="value" />.
+		/// </summary>
+		/// <param name="value">The value to return.</param>
+		/// <remarks>This method is a performance optimization.</remarks>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected T Choose<T>(Option<T> value)
+		{
+			return value.Result;
+		}
+
+		/// <summary>
+		///   Returns either <paramref name="value1" /> or <paramref name="value2" /> nondeterministically.
+		/// </summary>
+		/// <param name="value1">The first value to choose.</param>
+		/// <param name="value2">The second value to choose.</param>
+		/// <remarks>This method is a performance optimization.</remarks>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected T Choose<T>(Option<T> value1, Option<T> value2)
+		{
+			return _defaultChoice.Choose(value1, value2);
+		}
+
+		/// <summary>
+		///   Returns either <paramref name="value1" />, <paramref name="value2" />, or <paramref name="value3" /> nondeterministically.
+		/// </summary>
+		/// <param name="value1">The first value to choose.</param>
+		/// <param name="value2">The second value to choose.</param>
+		/// <param name="value3">The third value to choose.</param>
+		/// <remarks>This method is a performance optimization.</remarks>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected T Choose<T>(Option<T> value1, Option<T> value2, Option<T> value3)
+		{
+			return _defaultChoice.Choose(value1, value2, value3);
+		}
+
+		/// <summary>
+		///   Returns either <paramref name="value1" />, <paramref name="value2" />, <paramref name="value3" />, or
+		///   <paramref name="value4" /> nondeterministically.
+		/// </summary>
+		/// <param name="value1">The first value to choose.</param>
+		/// <param name="value2">The second value to choose.</param>
+		/// <param name="value3">The third value to choose.</param>
+		/// <param name="value4">The fourth value to choose.</param>
+		/// <remarks>This method is a performance optimization.</remarks>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected T Choose<T>(Option<T> value1, Option<T> value2, Option<T> value3, Option<T> value4)
+		{
+			return _defaultChoice.Choose(value1, value2, value3, value4);
+		}
+
+		/// <summary>
+		///   Returns either <paramref name="value1" />, <paramref name="value2" />, <paramref name="value3" />,
+		///   <paramref name="value4" />, or <paramref name="value5" /> nondeterministically.
+		/// </summary>
+		/// <param name="value1">The first value to choose.</param>
+		/// <param name="value2">The second value to choose.</param>
+		/// <param name="value3">The third value to choose.</param>
+		/// <param name="value4">The fourth value to choose.</param>
+		/// <param name="value5">The fifth value to choose.</param>
+		/// <remarks>This method is a performance optimization.</remarks>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected T Choose<T>(Option<T> value1, Option<T> value2, Option<T> value3, Option<T> value4, Option<T> value5)
+		{
+			return _defaultChoice.Choose(value1, value2, value3, value4, value5);
+		}
+
+		/// <summary>
+		///   Returns either <paramref name="value1" />, <paramref name="value2" />, <paramref name="value3" />,
+		///   <paramref name="value4" />, <paramref name="value5" />, or <paramref name="value6" /> nondeterministically.
+		/// </summary>
+		/// <param name="value1">The first value to choose.</param>
+		/// <param name="value2">The second value to choose.</param>
+		/// <param name="value3">The third value to choose.</param>
+		/// <param name="value4">The fourth value to choose.</param>
+		/// <param name="value5">The fifth value to choose.</param>
+		/// <param name="value6">The sixth value to choose.</param>
+		/// <remarks>This method is a performance optimization.</remarks>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected T Choose<T>(Option<T> value1, Option<T> value2, Option<T> value3, Option<T> value4, Option<T> value5, Option<T> value6)
+		{
+			return _defaultChoice.Choose(value1, value2, value3, value4, value5, value6);
+		}
+
+		/// <summary>
+		///   Returns one of the <paramref name="values" /> nondeterministically.
+		/// </summary>
+		/// <param name="values">The values to choose from.</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected T Choose<T>(params Option<T>[] values)
+		{
+			return _defaultChoice.Choose(values);
+		}
+
 	}
 }
