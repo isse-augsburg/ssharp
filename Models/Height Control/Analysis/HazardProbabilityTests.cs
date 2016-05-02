3786c60ebdd0814c5279391fc6da43dde6f96045
Johannes Leupolz <johannes.leupolz@googlemail.com>
2016-05-02 15:22:20 +0200

    Added probability calculation for height control and pressure tank

---
+++ Models/Height Control/Analysis/HazardProbabilityTests.cs
@@ -0,0 +1,59 @@
+﻿// The MIT License (MIT)
+// 
+// Copyright (c) 2014-2016, Institute for Software & Systems Engineering
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafetySharp.CaseStudies.HeightControl.Analysis
+{
+	using FluentAssertions;
+	using Modeling;
+	using NUnit.Framework;
+	using SafetySharp.Analysis;
+	using SafetySharp.Modeling;
+
+	class HazardProbabilityTests
+	{
+		[Test]
+		public void CalculateHazardInOriginalDesign()
+		{
+			var model = Model.CreateOriginal();
+			model.Vehicles.DisregardTrafficRules.ProbabilityOfOccurrence = Probability.Zero;
+			model.HeightControl.PreControl.PositionDetector.Misdetection.ProbabilityOfOccurrence = Probability.Zero;
+			model.HeightControl.PreControl.PositionDetector.FalseDetection.ProbabilityOfOccurrence = Probability.Zero;
+			model.HeightControl.MainControl.PositionDetector.Misdetection.ProbabilityOfOccurrence = Probability.Zero;
+			model.HeightControl.MainControl.PositionDetector.FalseDetection.ProbabilityOfOccurrence = Probability.Zero;
+			model.HeightControl.MainControl.LeftDetector.Misdetection.ProbabilityOfOccurrence = Probability.Zero;
+			model.HeightControl.MainControl.LeftDetector.FalseDetection.ProbabilityOfOccurrence = Probability.Zero;
+			model.HeightControl.MainControl.RightDetector.Misdetection.ProbabilityOfOccurrence = Probability.Zero;
+			model.HeightControl.MainControl.RightDetector.FalseDetection.ProbabilityOfOccurrence = Probability.Zero;
+			model.HeightControl.EndControl.LeftLaneDetector.Misdetection.ProbabilityOfOccurrence = Probability.Zero;
+			model.HeightControl.EndControl.LeftLaneDetector.FalseDetection.ProbabilityOfOccurrence = Probability.Zero;
+
+			var result = ModelChecker.CalculateProbabilityOfHazard(model, model.Collision);
+			Console.Write($"Probability of hazard: {result.Value}");
+		}
+	}
+}
