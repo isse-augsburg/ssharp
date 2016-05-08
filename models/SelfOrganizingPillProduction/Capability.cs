a6bba9a1d14d4d152e599a445319c97fb66b3913
maul.esel <maul.esel@go4more.de>
2016-05-08 16:17:13 +0200

    model capabilities in S#

---
+++ models/SelfOrganizingPillProduction/Capability.cs
@@ -0,0 +1,42 @@
+﻿namespace SelfOrganizingPillProduction
+{
+    abstract class Capability
+    {
+    }
+
+    /// <summary>
+    /// Represents the loading of empty pill containers on the conveyor belt.
+    /// </summary>
+    class ProduceCapability : Capability
+    {
+        private ProduceCapability() { }
+
+        public static readonly ProduceCapability Instance = new ProduceCapability();
+    }
+
+    /// <summary>
+    /// Represents the removal of pill containers from the conveyor belt, labeling and palletization.
+    /// </summary>
+    class ConsumeCapability : Capability
+    {
+        private ConsumeCapability() { }
+
+        public static readonly ConsumeCapability Instance = new ConsumeCapability();
+    }
+
+    /// <summary>
+    /// Represents the addition of a specified amount of a certain ingredient to the container.
+    /// </summary>
+    class Ingredient : Capability
+    {
+        public Ingredient(IngredientType type, uint amount)
+        {
+            Type = type;
+            Amount = amount;
+        }
+
+        public IngredientType Type { get; private set; }
+
+        public uint Amount { get; private set; }
+    }
+}
---
+++ models/SelfOrganizingPillProduction/IngredientType.cs
@@ -0,0 +1,6 @@
+﻿enum IngredientType
+{
+    BlueParticulate,
+    RedParticulate,
+    YellowParticulate
+}
