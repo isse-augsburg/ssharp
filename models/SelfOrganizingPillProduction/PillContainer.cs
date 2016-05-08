7db44ce0c8b96a452398641b23b6268b9358a2ab
maul.esel <maul.esel@go4more.de>
2016-05-08 16:53:12 +0200

    model pill containers and recipes

---
+++ models/SelfOrganizingPillProduction/PillContainer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using SafetySharp.Modeling;
+
+namespace SelfOrganizingPillProduction
+{
+    /// <summary>
+    /// A pill container which is filled with different ingredients.
+    /// </summary>
+    class PillContainer : Component
+    {
+        public PillContainer(Recipe recipe)
+        {
+            Recipe = recipe;
+            State = new List<Capability>(recipe.RequiredCapabilities.Length);
+        }
+
+        /// <summary>
+        /// The recipe according to which the container is processed.
+        /// </summary>
+        public Recipe Recipe { get; }
+
+        /// <summary>
+        /// The capabilities already applied to the container.
+        /// </summary>
+        public List<Capability> State { get; }
+
+        /// <summary>
+        /// Adds an ingredient to the container.
+        /// </summary>
+        /// <param name="ingredient"></param>
+        public void AddIngredient(Ingredient ingredient)
+        {
+            State.Add(ingredient);
+        }
+    }
+}
---
+++ models/SelfOrganizingPillProduction/Recipe.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SelfOrganizingPillProduction
+{
+    /// <summary>
+    /// Describes how a container should be processed.
+    /// </summary>
+    class Recipe
+    {
+        /// <summary>
+        /// Creates a new recipe with the specified sequence of ingredients.
+        /// </summary>
+        /// <param name="ingredients">The sequence of ingredients to add to the containers.</param>
+        /// <param name="amount">The number of containers to produce for this recipe.</param>
+        public Recipe(Ingredient[] ingredients, uint amount)
+        {
+            ActiveContainers = new List<PillContainer>((int)amount);
+            Amount = amount;
+            RequiredCapabilities = new Capability[] { ProduceCapability.Instance }
+                .Concat(ingredients)
+                .Concat(new[] { ConsumeCapability.Instance })
+                .ToArray();
+        }
+
+        /// <summary>
+        /// A list of all containers processed according to this recipe that are currently in the system.
+        /// </summary>
+        public List<PillContainer> ActiveContainers { get; }
+
+        /// <summary>
+        /// The sequence of capabilities defining this recipe.
+        /// </summary>
+        public Capability[] RequiredCapabilities { get;  }
+
+        /// <summary>
+        /// The total number of containers to be produced for this recipe.
+        /// </summary>
+        public uint Amount { get; }
+    }
+}
