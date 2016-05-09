0edb57da1b5e0cf9db083259a7e92505b6bb9049
maul.esel <maul.esel@go4more.de>
2016-05-09 12:49:49 +0200

    model PalletisationStation

---
+++ models/SelfOrganizingPillProduction/PalletisationStation.cs
@@ -0,0 +1,20 @@
+﻿namespace SelfOrganizingPillProduction
+{
+    /// <summary>
+    /// A production station that removes containers from the conveyor belt, closes, labels and stores them on pallets.
+    /// </summary>
+    class PalletisationStation : Station
+    {
+        public override Capability[] AvailableCapabilities { get; } = new[] { ConsumeCapability.Instance };
+
+        protected override void ExecuteRole(Role role)
+        {
+            Container.Recipe.ActiveContainers.Remove(Container);
+            if (Container.Recipe.ActiveContainers.Count == 0)
+            {
+                RemoveRecipeConfigurations(Container.Recipe);
+            }
+            Container = null;
+        }
+    }
+}
