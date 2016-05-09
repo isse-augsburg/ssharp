da0558af830edc155458aa3a5b913955046cb4c9
maul.esel <maul.esel@go4more.de>
2016-05-09 12:29:31 +0200

    model ParticulateDispenser station

---
+++ models/SelfOrganizingPillProduction/ParticulateDispenser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SelfOrganizingPillProduction
+{
+    /// <summary>
+    /// A production station that adds ingredients to the containers.
+    /// </summary>
+    class ParticulateDispenser : Station
+    {
+        private readonly Dictionary<IngredientType, uint> availableIngredients = new Dictionary<IngredientType, uint>();
+
+        public override Capability[] AvailableCapabilities
+        {
+            get
+            {
+                return availableIngredients.Select(kv => new Ingredient(type: kv.Key, amount: kv.Value)).ToArray();
+            }
+        }
+
+        protected override void ExecuteRole(Role role)
+        {
+            foreach (var capability in role.CapabilitiesToApply)
+            {
+                var ingredient = capability as Ingredient;
+                if (ingredient == null)
+                    throw new InvalidOperationException("Invalid capability in ParticulateDispenser: " + capability);
+                if (!availableIngredients.ContainsKey(ingredient.Type) || availableIngredients[ingredient.Type] < ingredient.Amount)
+                    throw new InvalidOperationException("Insufficient amount available of ingredient " + ingredient.Type);
+
+                availableIngredients[ingredient.Type] -= ingredient.Amount;
+                Container.AddIngredient(ingredient);
+            }
+        }
+    }
+}
