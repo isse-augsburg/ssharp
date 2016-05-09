4a0c2ec81c4e52118f2f2fdd2405ff3c8e2b16d1
maul.esel <maul.esel@go4more.de>
2016-05-09 10:59:17 +0200

    model roles and conditions in S#

---
+++ models/SelfOrganizingPillProduction/Condition.cs
@@ -0,0 +1,23 @@
+﻿namespace SelfOrganizingPillProduction
+{
+    /// <summary>
+    /// Describes the condition of a pill container before or after a role's capabilities are applied to it.
+    /// </summary>
+    class Condition
+    {
+        /// <summary>
+        /// The station the container is received from or sent to.
+        /// </summary>
+        public Station Port { get; set; }
+
+        /// <summary>
+        /// The capabilities already applied to the container.
+        /// </summary>
+        public Capability[] State { get; set; }
+
+        /// <summary>
+        /// A reference to the container's recipe.
+        /// </summary>
+        public Recipe Recipe { get; set; }
+    }
+}
---
+++ models/SelfOrganizingPillProduction/Role.cs
@@ -0,0 +1,28 @@
+﻿namespace SelfOrganizingPillProduction
+{
+    /// <summary>
+    /// Describes a sequence of capabilities a specific station should apply to a container.
+    /// </summary>
+    class Role
+    {
+        /// <summary>
+        /// The condition of the container before the role is executed.
+        /// </summary>
+        public Condition PreCondition { get; set; }
+
+        /// <summary>
+        /// The condition of the container after the role is executed.
+        /// </summary>
+        public Condition PostCondition { get; set; }
+
+        /// <summary>
+        /// The capabilities to apply.
+        /// </summary>
+        public Capability[] CapabilitiesToApply { get; set; }
+
+        /// <summary>
+        /// The recipe the role belongs to.
+        /// </summary>
+        public Recipe Recipe => (PreCondition ?? PostCondition).Recipe;
+    }
+}
