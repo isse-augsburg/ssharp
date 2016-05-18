be0e8609bf1a0a74b82e57621c947eb7952c4ecb
maul.esel <maul.esel@go4more.de>
2016-05-18 15:47:07 +0200

    move fault renaming to helper class

---
+++ Models/SelfOrganizingPillProduction/Modeling/FaultHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using SafetySharp.Modeling;
+
+namespace SafetySharp.CaseStudies.SelfOrganizingPillProduction.Modeling
+{
+    internal static class FaultHelper
+    {
+        internal static void PrefixFaultNames(Component component, string prefix)
+        {
+            var faults = (from field in component.GetType().GetFields()
+                          where typeof(Fault).IsAssignableFrom(field.FieldType)
+                          select field.GetValue(component)).Cast<Fault>();
+            foreach (var fault in faults)
+            {
+                fault.Name = $"{prefix}.{fault.Name}";
+            }
+        }
+    }
+}
