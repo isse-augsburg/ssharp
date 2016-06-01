82e093477fcc399053150692a55bf7737fa2c85c
maul.esel <maul.esel@go4more.de>
2016-06-01 14:39:59 +0200

    Add another heuristic: some redundancy remains

---
+++ Source/SafetySharp/Analysis/Heuristics/RedundanyRemainingHeuristic.cs
@@ -0,0 +1,49 @@
+﻿namespace SafetySharp.Analysis.Heuristics
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Modeling;
+    using Runtime;
+
+    public class RedundanyRemainingHeuristic : IFaultSetHeuristic
+    {
+        private readonly IEnumerable<Fault>[] faultGroups;
+        private readonly Fault[] allFaults;
+        private readonly ISet<FaultSet> noSuggestions = new HashSet<FaultSet>();
+
+        public RedundanyRemainingHeuristic(ModelBase model, params IEnumerable<Fault>[] faultGroups)
+        {
+            this.faultGroups = faultGroups;
+            allFaults = model.Faults;
+        }
+
+        public ISet<FaultSet> MakeSuggestions(ISet<FaultSet> setsToCheck)
+        {
+            // only make suggestions at the beginning
+            if (setsToCheck.Count != 1 || !setsToCheck.First().IsEmpty)
+                return noSuggestions;
+
+            var suggestions = new HashSet<FaultSet>();
+            var allFaults = new FaultSet(this.allFaults);
+
+            foreach (var excludedFaults in CartesianProduct(faultGroups))
+            {
+                var subsuming = new FaultSet(excludedFaults.ToArray()).SubsumingFaults();
+                suggestions.Add(allFaults.GetDifference(subsuming));
+            }
+
+            return suggestions;
+        }
+
+        public static IEnumerable<IEnumerable<T>> CartesianProduct<T>(IEnumerable<IEnumerable<T>> sequences)
+        {
+            IEnumerable<IEnumerable<T>> emptyProduct = new[] { Enumerable.Empty<T>() };
+            return sequences.Aggregate(
+              emptyProduct,
+              (accumulator, sequence) =>
+                from accseq in accumulator
+                from item in sequence
+                select accseq.Concat(new[] { item }));
+        }
+    }
+}
