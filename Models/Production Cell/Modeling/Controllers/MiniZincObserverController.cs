42b2fb755b505c47f64f20856422731fcf1de193
Benedikt Eberhardinger <benedikt.eberhardinger@informatik.uni-augsburg.de>
2016-06-20 15:32:51 +0200

    Oracle as Exception added

--- Models/Production Cell/Modeling/Controllers/MiniZincObserverController.cs
+++ Models/Production Cell/Modeling/Controllers/MiniZincObserverController.cs
@@ -120,14 +120,20 @@ namespace SafetySharp.CaseStudies.ProductionCell.Modeling.Controllers
 				agent.OnReconfigured();
 			}
 
+		    var isReconfPossible = IsReconfPossible(Agents.OfType<RobotAgent>(), Agents.OfType<CartAgent>(), Tasks, this);
+
 			var lines = File.ReadAllLines(ConfigurationFile);
 			if (lines[0].Contains("UNSATISFIABLE"))
 			{
 				ReconfigurationState = ReconfStates.Failed;
+                if (isReconfPossible) 
+                    throw new Exception("Reconf. failed while there is a solution.");
 				return;
 			}
 
 			ReconfigurationState = ReconfStates.Succedded;
+            if (!isReconfPossible)
+                throw new Exception("Reconf. is succedded while there is no reconf. possible.");
 
 			var roleAllocations = Parse(lines[0], lines[1]).ToArray();
 			for (var i = 0; i < roleAllocations.Length; i++)
@@ -211,5 +217,92 @@ namespace SafetySharp.CaseStudies.ProductionCell.Modeling.Controllers
 
 			return line.Substring(openBrace + 1, closeBrace - openBrace - 1).Split(',').Select(n => Int32.Parse(n.Trim()) - 1).ToArray();
 		}
-	}
+
+        private bool IsReconfPossible(IEnumerable<RobotAgent> robotsAgents, IEnumerable<CartAgent> cartAgents, IEnumerable<Task> tasks,
+                                      ObserverController observerController)
+        {
+            var isReconfPossible = true;
+            var matrix = GetConnectionMatrix(robotsAgents);
+
+            foreach (var task in tasks)
+            {
+                isReconfPossible &= task.Capabilities.All(capability => robotsAgents.Any(agent => agent.AvailableCapabilites.Contains(capability)));
+                if (!isReconfPossible)
+                    break;
+
+                var candidates = robotsAgents.Where(agent => agent.AvailableCapabilites.Contains(task.Capabilities.First())).ToArray();
+
+                for (var i = 0; i < task.Capabilities.Length - 1; i++)
+                {
+                    candidates =
+                        candidates.SelectMany<RobotAgent, RobotAgent>(r => matrix[r])
+                                  .Where(r => r.AvailableCapabilites.Contains(task.Capabilities[i + 1]))
+                                  .ToArray();
+                    if (candidates.Length == 0)
+                    {
+                        isReconfPossible = false;
+                        goto end;
+                    }
+                }
+            }
+
+            end:
+
+            if (isReconfPossible == observerController.ReconfigurationState.Equals(ReconfStates.Failed))
+            {
+                var agents = robotsAgents.Cast<Agent>().Concat(cartAgents).ToArray();
+                using (var writer = new StreamWriter("counterFile"))
+                {
+                    var isCart = String.Join(",", agents.Select(a => (a is CartAgent).ToString().ToLower()));
+                    var capabilities = String.Join(",", agents.Select(a =>
+                        $"{{{String.Join(",", a.AvailableCapabilites.Select(c => c.Identifier))}}}"));
+                    var isConnected = String.Join("\n|", agents.Select(from =>
+                        String.Join(",", agents.Select(to => (from.Outputs.Contains(to) || from == to).ToString().ToLower()))));
+
+                    writer.WriteLine($"noAgents = {agents.Length};");
+                    writer.WriteLine($"capabilities = [{capabilities}];");
+                    writer.WriteLine($"isCart = [{isCart}];");
+                    writer.WriteLine($"isConnected = [|{isConnected}|]");
+                }
+            }
+
+            return isReconfPossible;
+        }
+
+        private Dictionary<RobotAgent, List<RobotAgent>> GetConnectionMatrix(IEnumerable<RobotAgent> robotAgents)
+        {
+            var matrix = new Dictionary<RobotAgent, List<RobotAgent>>();
+
+            foreach (var robot in robotAgents)
+            {
+                var list = new List<RobotAgent>(robotAgents.Where(r => IsConnected(robot, r, new HashSet<RobotAgent>())));
+                matrix.Add(robot, list);
+            }
+
+            return matrix;
+        }
+
+        private bool IsConnected(RobotAgent source, RobotAgent target, HashSet<RobotAgent> seenRobots)
+        {
+            if (source == target)
+                return true;
+
+            if (!seenRobots.Add(source))
+                return false;
+
+            foreach (var output in source.Outputs)
+            {
+                foreach (var output2 in output.Outputs)
+                {
+                    if (output2 == target)
+                        return true;
+
+                    if (IsConnected((RobotAgent)output2, target, seenRobots))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
 }
