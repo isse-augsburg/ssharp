6ef9551ce35c0fbe57a5fd8dd196abddbd9dde20
maul.esel <maul.esel@go4more.de>
2016-06-23 18:58:42 +0200

    add FaultSetCollection class

---
+++ Source/SafetySharp/Analysis/FaultSetCollection.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SafetySharp.Runtime;
+
+namespace SafetySharp.Analysis
+{
+	class FaultSetCollection
+	{
+		private readonly int numFaults;
+		private readonly HashSet<FaultSet>[] elementsByCardinality;
+
+		public FaultSetCollection(int numFaults)
+		{
+			this.numFaults = numFaults;
+			elementsByCardinality = new HashSet<FaultSet>[numFaults + 1];
+		}
+
+		public void Add(FaultSet set)
+		{
+			var cardinality = set.Cardinality;
+			if (elementsByCardinality[cardinality] == null)
+				elementsByCardinality[cardinality] = new HashSet<FaultSet>();
+			elementsByCardinality[cardinality].Add(set);
+		}
+
+		public bool ContainsSubsetOf(FaultSet set)
+		{
+			var cardinality = set.Cardinality;
+			if (elementsByCardinality[cardinality]?.Contains(set) ?? false)
+				return true;
+
+			for (int i = 0; i < cardinality; ++i)
+			{
+				if (elementsByCardinality[cardinality]?.Any(other => other.IsSubsetOf(set)) ?? false)
+					return true;
+			}
+			return false;
+		}
+
+		public bool ContainsSupersetOf(FaultSet set)
+		{
+			var cardinality = set.Cardinality;
+			if (elementsByCardinality[cardinality]?.Contains(set) ?? false)
+				return true;
+
+			for (int i = numFaults; i > cardinality; --i)
+			{
+				if (elementsByCardinality[cardinality]?.Any(other => set.IsSubsetOf(other)) ?? false)
+					return true;
+			}
+			return false;
+		}
+	}
+}
