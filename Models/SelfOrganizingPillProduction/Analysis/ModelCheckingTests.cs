da04cddc7859a4c5aed92974ab0cce69379f4628
maul.esel <maul.esel@go4more.de>
2016-06-28 18:34:47 -0700

    add one NUnit test for all parameter combinations

--- Models/SelfOrganizingPillProduction/Analysis/ModelCheckingTests.cs
+++ Models/SelfOrganizingPillProduction/Analysis/ModelCheckingTests.cs
@@ -10,54 +10,48 @@ namespace SafetySharp.CaseStudies.SelfOrganizingPillProduction.Analysis
 {
     public class ModelCheckingTests
     {
-        [Test]
-        public void SimpleDcca()
+        public enum HeuristicsUsage { None, Subsumption, Redundancy, Both }
+
+        [Test, Combinatorial]
+        public void DccaTest(
+            [Values("simple_setup.model", "simple_setup3.model", "simple_setup2.model", "medium_setup.model", "complex_setup.model")]
+            string modelFile,
+            [Values(HeuristicsUsage.None, HeuristicsUsage.Subsumption, HeuristicsUsage.Redundancy, HeuristicsUsage.Both)]
+            HeuristicsUsage heuristicsUsage,
+            [Values(FaultActivationBehaviour.ForceOnly, FaultActivationBehaviour.ForceThenFallback, FaultActivationBehaviour.Nondeterministic)]
+            FaultActivationBehaviour faultActivation
+        )
         {
-            var model = new ModelSetupParser().Parse("Analysis/simple_setup.model");
-            Dcca(model, new SubsumptionHeuristic(model), RedundancyHeuristic(model));
+            var model = new ModelSetupParser().Parse($"Analysis/{modelFile}");
+            IFaultSetHeuristic[] heuristics;
+            switch (heuristicsUsage)
+            {
+                case HeuristicsUsage.None:
+                    heuristics = new IFaultSetHeuristic[0];
+                    break;
+                case HeuristicsUsage.Subsumption:
+                    heuristics = new[] { new SubsumptionHeuristic(model) };
+                    break;
+                case HeuristicsUsage.Redundancy:
+                    heuristics = new[] { RedundancyHeuristic(model) };
+                    break;
+                default:
+                case HeuristicsUsage.Both:
+                    heuristics = new[] { new SubsumptionHeuristic(model), RedundancyHeuristic(model) };
+                    break;
+            }
+            Dcca(model, faultActivation, heuristics);
         }
 
-        [Test]
-        public void SimpleDccaWithoutHeuristics()
-        {
-            Dcca(new ModelSetupParser().Parse("Analysis/simple_setup.model"));
-        }
-
-        [Test]
-        public void MediumDcca()
-        {
-            var model = new ModelSetupParser().Parse("Analysis/medium_setup.model");
-            Dcca(model, new SubsumptionHeuristic(model), RedundancyHeuristic(model));
-        }
-
-        [Test]
-        public void MediumDccaWithoutHeuristics()
-        {
-            Dcca(new ModelSetupParser().Parse("Analysis/medium_setup.model"));
-        }
-
-        [Test]
-        public void ComplexDcca()
-        {
-            var model = new ModelSetupParser().Parse("Analysis/complex_setup.model");
-            Dcca(model, new SubsumptionHeuristic(model), RedundancyHeuristic(model));
-        }
-
-        [Test]
-        public void ComplexDccaWithoutHeuristics()
-        {
-            Dcca(new ModelSetupParser().Parse("Analysis/complex_setup.model"));
-        }
-
-        private void Dcca(Model model, params IFaultSetHeuristic[] heuristics)
+        private void Dcca(Model model, FaultActivationBehaviour activation, params IFaultSetHeuristic[] heuristics)
         {
             var modelChecker = new SafetyAnalysis();
 
-            modelChecker.Configuration.StateCapacity = 40000;
+            modelChecker.Configuration.StateCapacity = 1 << 20;
             modelChecker.Configuration.CpuCount = 1;
             modelChecker.Configuration.GenerateCounterExample = false;
             modelChecker.Heuristics.AddRange(heuristics);
-            modelChecker.FaultActivationBehaviour = FaultActivationBehaviour.ForceOnly;
+            modelChecker.FaultActivationBehaviour = activation;
 
             var result = modelChecker.ComputeMinimalCriticalSets(model, model.ObserverController.Unsatisfiable);
             System.Console.WriteLine(result);
