4e2b90067b1d3e93537362ae6572a27a362b3ad9
maul.esel <maul.esel@go4more.de>
2016-10-28 00:14:52 +0200

    simple test using MicrostepScheduler

---
+++ Tests/Execution/Simulation/microstep parallelism.cs
@@ -0,0 +1,59 @@
+﻿// The MIT License (MIT)
+// 
+// Copyright (c) 2014-2016, Institute for Software & Systems Engineering
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+namespace Tests.Execution.Simulation
+{
+	using System.Threading.Tasks;
+	using SafetySharp.Analysis;
+	using SafetySharp.Modeling;
+	using Shouldly;
+	using Utilities;
+
+	internal class MicrostepParallelism : TestObject
+	{
+		protected override void Check()
+		{
+			var simulator = new Simulator(TestModel.InitializeModel(new C(), new C()));
+			simulator.SimulateStep();
+			C.counter.ShouldBe(4);
+		}
+
+		private class C : Component
+		{
+			public static int counter = 0;
+
+			public override void Update()
+			{
+				MicrostepScheduler.Schedule(AsyncMethod);
+			}
+
+			private async Task AsyncMethod()
+			{
+				int _ctr = ++counter;
+				await Task.Yield();
+
+				if (counter - _ctr == 1)
+					_ctr = ++counter;
+			}
+		}
+	}
+}
