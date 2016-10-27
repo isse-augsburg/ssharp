b9556e5cd28012218624fc6c3a0dd4ed0d30c149
maul.esel <maul.esel@go4more.de>
2016-10-28 00:13:21 +0200

    implement MicrostepScheduler

---
+++ Source/SafetySharp/Modeling/MicrostepScheduler.cs
@@ -0,0 +1,79 @@
+﻿// The MIT License (MIT)
+// 
+// Copyright (c) 2014-2016, Institute for Software & Systems Engineering
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+namespace SafetySharp.Modeling
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Threading;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// This class can be used to simulate parallel execution within a microstep.
+	/// </summary>
+	public static class MicrostepScheduler
+	{
+		[ThreadStatic]
+		private static SingleThreadSynchronizationContext _context
+			= new SingleThreadSynchronizationContext();
+
+		/// <summary>
+		/// Schedules a callback for simulated asynchronous execution.
+		/// </summary>
+		/// <param name="action">The scheduled callback, which may be marked as <c>async</c>.</param>
+		public static void Schedule(Func<Task> action)
+		{
+			var _oldContext = SynchronizationContext.Current;
+			SynchronizationContext.SetSynchronizationContext(_context);
+			var task = action();
+			SynchronizationContext.SetSynchronizationContext(_oldContext);
+			_context.Post(o => task.Wait(), null);
+		}
+
+		/// <summary>
+		/// Processes the scheduled callbacks.
+		/// </summary>
+		internal static void CompleteSchedule()
+		{
+			_context.Run();
+		}
+
+		private class SingleThreadSynchronizationContext : SynchronizationContext
+		{
+			private readonly Queue<Tuple<SendOrPostCallback, object>> _queue = new Queue<Tuple<SendOrPostCallback, object>>();
+
+			public override void Post(SendOrPostCallback d, object state)
+			{
+				_queue.Enqueue(Tuple.Create(d, state));
+			}
+
+			public void Run()
+			{
+				while (_queue.Count > 0)
+				{
+					var job = _queue.Dequeue();
+					job.Item1(job.Item2);
+				}
+			}
+		}
+	}
+}
