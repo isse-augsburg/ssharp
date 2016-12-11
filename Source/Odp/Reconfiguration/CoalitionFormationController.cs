19f55ab1d091f62621db3c75415b88977da28b43
maul.esel <maul.esel@go4more.de>
2016-12-11 16:05:08 +0100

    some work on implementation of coalition-based reconf

--- Source/Odp/Reconfiguration/CoalitionFormationController.cs
+++ Source/Odp/Reconfiguration/CoalitionFormationController.cs
@@ -23,42 +23,160 @@
 namespace SafetySharp.Odp.Reconfiguration
 {
 	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics;
+	using System.Linq;
 	using System.Threading.Tasks;
 
+	/// <summary>
+	/// A decentralized, local reconfiguration algorithm based on coalition formation.
+	/// This class is only meant to be used by the <see cref="CoalitionReconfigurationAgent"/> class
+	/// (and possibly subclasses). It is implemented as an <see cref="IController"/> nevertheless in
+	/// order to allow compositional usage within other controllers.
+	/// 
+	/// (cf. Konstruktion selbst-organisierender Softwaresysteme, chapter 8)
+	/// </summary>
 	public class CoalitionFormationController : AbstractController
 	{
 		public CoalitionFormationController(BaseAgent[] agents) : base(agents) { }
 
-		public override Task<ConfigurationUpdate> CalculateConfigurations(object context, params ITask[] tasks)
+		public override async Task<ConfigurationUpdate> CalculateConfigurations(object context, params ITask[] tasks)
 		{
-			//if (state.isRequest) // also handle (IsRequest && IsLocalViolation)
+			Debug.Assert(tasks.Length == 1);
+			var task = tasks[0];
+
+			var leader = (CoalitionReconfigurationAgent)context;
+			var config = new ConfigurationUpdate();
+			var coalition = new Coalition(leader, task);
+
+			foreach (var predicate in  leader.BaseAgentState.ViolatedPredicates)
+				await SolveInvariantViolation(coalition, predicate, task, config);
+
+			return config;
+		}
+
+		/// <summary>
+		/// Selects the strategy used to solve the occuring invariant violations based on the violated predicate.
+		/// </summary>
+		protected virtual Task SolveInvariantViolation(Coalition coalition, InvariantPredicate invariant,
+																			 ITask task, ConfigurationUpdate config)
+		{
+			if (invariant == Invariant.CapabilityConsistency)
+				return RestoreCapabilityConsistency(coalition, task, config);
+
+			throw new NotImplementedException(); // TODO: strategies for other invariant predicates
+		}
+
+		/// <summary>
+		/// Reconfiguration strategy for violations of the <see cref="Invariant.CapabilityConsistency"/> invariant predicate.
+		/// </summary>
+		protected async Task RestoreCapabilityConsistency(Coalition coalition, ITask task, ConfigurationUpdate config)
+		{
+			while (true)
 			{
-				// if CurrentCoalition == null
-				//	join
-				// else
-				//	merge
+				await EnlargeCoalitionUntil(coalition.CapabilitiesSatisfied, coalition, RecrutableMembers(coalition));
+
+				foreach (var distribution in CalculateCapabilityDistributions(coalition))
+				{
+					var resourceFlow = CalculateResourceFlow(coalition, distribution);
+					if (resourceFlow != null)
+					{
+						await CalculateRoleAllocations(coalition, distribution, resourceFlow, config);
+						return;
+					}
+				}
+
+				// TODO: enlarge coalition to allow resource flow (?)
 			}
-			//else if (state.isLocalViolation)
+		}
+
+		private IEnumerable<BaseAgent> RecrutableMembers(Coalition coalition)
+		{
+			var memberBaseAgents = coalition.Members.Select(member => member.BaseAgent);
+
+			// TODO: prioritize already participating agents (inputs/outputs of member roles for task)
+			var queueIn = new Queue<BaseAgent>(coalition.Members.SelectMany(member => member.BaseAgentState.Inputs).Except(memberBaseAgents));
+			var queueOut = new Queue<BaseAgent>(coalition.Members.SelectMany(member => member.BaseAgentState.Outputs).Except(memberBaseAgents));
+
+			while (queueOut.Count > 0 || queueIn.Count > 0)
 			{
-				// determine required capabilities
-				// start coalition
-				// invite coalition neighbors until required capabilities are satisfied
-				// calculate TFR
-				// invite edge agents where necessary
-				// calculate new role assignment
-				// all coalition members update base agent configurations
-				// (synchronize)
-				// all coalition members send Go() to base agents
-				// coalition disbanded, reconf agents forgotten
+				// TODO: update BOTH queues (remove nextMember, add nextMember's neighbours, prioritize participating agents)
+				if (queueIn.Count > 0)
+					yield return queueIn.Dequeue();
+				if (queueOut.Count > 0)
+					yield return queueOut.Dequeue();
 			}
-			//else // agent breakdown
+		}
+
+		private async Task EnlargeCoalitionUntil(Func<bool> condition, Coalition coalition, IEnumerable<BaseAgent> possibleMembers)
+		{
+			foreach (var agent in possibleMembers.TakeWhile(a => !condition()))
+				await Invite(agent, coalition);
+		}
+
+		/// <summary>
+		/// Invites a new member into the coalition. The returned task completes when the invite has been answered.
+		/// </summary>
+		protected async Task<CoalitionReconfigurationAgent> Invite(BaseAgent agent, Coalition coalition)
+		{
+			await agent.RequestReconfiguration(coalition.Leader, coalition.Task);
+
+			// TODO: wait for invite reply - how? invitee might use await as well (TaskCompletionSource?)
+			// TODO: invitation might lead to coalition merge -- if no longer leader, stop here!?
+			// TODO: (implementation: cancellation token?)
+
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Lazily calculates all possible distributions on the capabilities in CTF between the members of the coalition.
+		/// </summary>
+		protected IEnumerable<BaseAgent[]> CalculateCapabilityDistributions(Coalition coalition)
+		{
+			var distribution = new BaseAgent[coalition._ctfEnd - coalition._ctfStart];
+			return CalculateCapabilityDistributions(coalition, distribution, 0);
+		}
+
+		// enumerate all paths, but lazily! (depth-first search)
+		private IEnumerable<BaseAgent[]> CalculateCapabilityDistributions(Coalition coalition, BaseAgent[] distribution, int prefixLength)
+		{
+			// termination case: copy distribution and return it
+			if (prefixLength == distribution.Length)
 			{
-				// question (not invite) non-neighbors until successor/predecessor of failed agent discovered
-				// (this requires knowledge of non-neighboring agents, or maybe a broadcast?)
-				// follow resource flow until closest living neighbor of failed agent found
-				// coalition clash -> merge, select new leader
-				// continue as described above
+				var result = new BaseAgent[distribution.Length];
+				Array.Copy(distribution, result, distribution.Length);
+				yield return result;
+				yield break;
 			}
+
+			// recursive case: iterate through all possible next agents, recurse, forward results
+			var eligibleAgents = coalition.Members
+										  .Select(member => member.BaseAgent)
+										  .Where(agent => CanSatisfyNext(agent, coalition, distribution, prefixLength));
+			foreach (var agent in eligibleAgents)
+			{
+				distribution[prefixLength] = agent;
+				foreach (var result in CalculateCapabilityDistributions(coalition, distribution, prefixLength + 1))
+					yield return result;
+			}
+		}
+
+		// TODO: override for pill production
+		protected virtual bool CanSatisfyNext(BaseAgent agent, Coalition coalition, BaseAgent[] distribution, int prefixLength)
+		{
+			var capability = coalition.Task.RequiredCapabilities[coalition._ctfStart + prefixLength];
+			return agent.AvailableCapabilities.Contains(capability);
+		}
+
+		protected BaseAgent[] CalculateResourceFlow(Coalition coalition, BaseAgent[] distribution)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected async Task CalculateRoleAllocations(Coalition coalition, BaseAgent[] distribution, BaseAgent[] resourceFlow,
+												ConfigurationUpdate config)
+		{
+			await Task.Yield();
 			throw new NotImplementedException();
 		}
 	}
