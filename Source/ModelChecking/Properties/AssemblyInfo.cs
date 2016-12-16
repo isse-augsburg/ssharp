a0e86ede38116c5f19cdd9619af3f2d48bf975eb
Johannes Leupolz <johannes.leupolz@googlemail.com>
2016-12-16 11:25:56 +0100

    Refactoring: Added new project ModelChecking which should be responsible for every model checking activity of generic executable models (even non S# ones).

---
+++ Source/ModelChecking/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("S# ModelChecking")]
+[assembly: InternalsVisibleTo("SafetySharp.Compiler")]
+[assembly: InternalsVisibleTo("SafetySharp.Tests")]
+[assembly: InternalsVisibleTo("SafetySharp.LtsMin")]
+[assembly: InternalsVisibleTo("SafetySharp.Modeling")]
