aa047c24990f1e2b19b2303d213bdeb56353136c
maul.esel <maul.esel@go4more.de>
2017-03-15 11:38:09 -0700

    add csv header

--- Benchmark/testingKnownErrorsEvaluation.ps1
+++ Benchmark/testingKnownErrorsEvaluation.ps1
@@ -51,6 +51,7 @@ if (Test-Path -Path $csv_path)
 {
 	rm $csv_path
 }
+Add-Content $csv_path "fault,analysis mode,model name,thrown exception,# faults,required time (ms),# checked sets,% checked sets,# trivial checks,# suggestions,% good suggestions,% bad suggestions,# mininal-critical sets,avg. cardinality,min. cardinality,max. cardinality`r`n"
 
 $symbols=@("ENABLE_F1", "ENABLE_F2", "ENABLE_F4", "ENABLE_F5", "ENABLE_F6", "ENABLE_F7", "NO_ERRORS")
 
