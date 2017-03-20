769a3a3a74c642067d86b4f49b68e6be6be431ac
Johannes Leupolz <johannes.leupolz@googlemail.com>
2017-03-20 11:15:32 +0100

    Add separate project for SafetyCheckingTests

--- Source/SafetyChecking/Properties/AssemblyInfo.cs
+++ Source/SafetyChecking/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("S# ModelChecking")]
+[assembly: InternalsVisibleTo("ISSE.SafetyChecking.Tests")]
 [assembly: InternalsVisibleTo("SafetySharp.Compiler")]
 [assembly: InternalsVisibleTo("SafetySharp.Tests")]
 [assembly: InternalsVisibleTo("SafetySharp.LtsMin")]
