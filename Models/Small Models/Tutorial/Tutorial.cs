efc2f8cae9c031f44b44751a56fe74f4705271e1
Johannes Leupolz <johannes.leupolz@googlemail.com>
2017-07-12 18:08:18 +0200

    Small models: Add entrance point for tutorial

---
+++ Models/Small Models/Tutorial/Tutorial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SafetySharp.CaseStudies.SmallModels.Tutorial
+{
+	public class NewModel
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("hello world");
+		}
+	}
+}
