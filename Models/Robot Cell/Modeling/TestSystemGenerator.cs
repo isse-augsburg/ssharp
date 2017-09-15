2f58dc457daaa65c2a65052f47d4728d2f2b8334
Benedikt Eberhardinger <benedikt.eberhardinger@informatik.uni-augsburg.de>
2017-09-15 14:23:00 +0200

    Test System Generator implemented

---
+++ Models/Robot Cell/Modeling/TestSystemGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafetySharp.CaseStudies.RobotCell.Modeling
+{
+    public class TestSystemGenerator
+    {
+
+        public Tuple<HashSet<DummyAgent>, List<DummyCapability>> Generate(int sysSize, int capCount, int ioCount)
+        {
+            // generate agents
+            var agentSet = new HashSet<DummyAgent>();
+            for (var i = 0; i < sysSize; i++)
+                agentSet.Add(new DummyAgent(i));
+
+            // generate task
+            var task = new List<DummyCapability>();
+            for (var i = 0; i < sysSize; i++)
+            {
+                var p1 = "";
+                var p0 = ((char)('A' + (i % 26))).ToString();
+
+                if (i >= 26)
+                {
+                    p1 = ((char)('A' + (int)(i / 26) - 1)).ToString();
+                }
+
+                task.Add(new DummyCapability(p1 + p0));
+            }
+
+            // Will hold the result
+            HashSet<DummyAgent> agents = null;
+
+            // generate capabilities
+            var agentCapGenerator = new AgentCapabilityGenerator(capCount, agentSet, task);
+
+            agents = agentCapGenerator.Generate();
+
+            
+            // generate ios
+            var iogGenerator = new IOGraphGenerator(ioCount, agents);
+            agents = iogGenerator.Generate();
+
+            return new Tuple<HashSet<DummyAgent>,List<DummyCapability>>(agents, task);
+        }
+
+    }
+}
