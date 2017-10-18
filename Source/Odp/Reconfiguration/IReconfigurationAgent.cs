e08bda4c550145405574cb0f5d5b5bc3bc142eea
maul.esel <maul.esel@go4more.de>
2017-10-18 11:46:26 +0200

    document IReconfigurationAgent

--- Source/Odp/Reconfiguration/IReconfigurationAgent.cs
+++ Source/Odp/Reconfiguration/IReconfigurationAgent.cs
@@ -22,9 +22,41 @@
 
 namespace SafetySharp.Odp.Reconfiguration
 {
+	/// <summary>
+	///   An interface implemented by reconfiguration agents for (partially) distributed reconfiguration.
+	/// </summary>
+	/// <remarks>
+	///   One reconfiguration agent is associated to exactly one <see cref="BaseAgent"/>
+	///   and is responsible for reconfiguring exactly one <see cref="ITask"/>.
+	///   The reconfiguration agent must not communicate with the <see cref="BaseAgent"/>
+	///   directly, but via a <see cref="ReconfigurationAgentHandler"/> passed to its constructor.
+	/// </remarks>
 	public interface IReconfigurationAgent : IAgent
 	{
+		/// <summary>
+		///   Called to notify the reconfiguration agent, that its <see cref="BaseAgent"/>
+		///   requested it reconfigure the given <see cref="task"/>.
+		/// </summary>
+		/// <remarks>
+		///   This may be called because the agent detected an invariant violation,
+		///   or for an initial configuration for the <paramref name="task"/>,
+		///   or because another agent requested its participation in an ongoing reconfiguration.
+		///   It may be called several times during the lifetime of the reconfiguration agent
+		///   - at most once for either of the first two reasons, but an unlimited number of times
+		///   for the last reason.
+		/// </remarks>
+		/// <param name="task">The task for which the reconfiguration agent is responsible.</param>
+		/// <param name="agent">
+		///   The agent causing the call. This is either the associated base agent,
+		///   or another reconf agent that requested participation.
+		/// </param>
+		/// <param name="baseAgentState">The state of the base agent as well as additional information about the reason for the call.</param>
 		void StartReconfiguration(ITask task, IAgent agent, BaseAgent.State baseAgentState);
+
+		/// <summary>
+		///   Called by the <see cref="ReconfigurationAgentHandler"/> in response to <see cref="ReconfigurationAgentHandler.UpdateAllocatedRoles"/>.
+		///   Notifies the reconfiguration agent that the requested updates have been successfully applied.
+		/// </summary>
 		void Acknowledge();
 	}
 }
