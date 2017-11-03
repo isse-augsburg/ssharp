2561a6198ee01593b7e6d6fc79567c896b89cb9e
gebharal <gebharal@SWT144>
2017-11-03 13:15:50 +0100

    Set up visualization project

---
+++ Models/Visualizations/RobotCell.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SafetySharp.CaseStudies.Visualizations
+{
+    /// <summary>
+    /// Interaktionslogik für RobotCell.xaml
+    /// </summary>
+    public partial class RobotCell
+    { 
+        public RobotCell()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void OnModelStateReset()
+        {
+
+        }
+
+        private void UpdateModelState() {
+
+        }
+    }
+}
