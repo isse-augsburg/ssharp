b1315110ce12fbe0ac24d754da78a812ce6a4a81
Stefan Fritsch <stef.fritsch@gmx.de>
2017-11-14 19:23:14 +0100

    Simple bayesian example model for constraint and score based analysis

---
+++ Models/Small Models/SimpleBayesianExample/BayesianAnalysis.cs
@@ -0,0 +1,99 @@
+﻿namespace SafetySharp.CaseStudies.SmallModels.SimpleBayesianExample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Analysis;
+    using Bayesian;
+    using ISSE.SafetyChecking;
+    using ISSE.SafetyChecking.MinimalCriticalSetAnalysis;
+    using ModelChecking;
+    using NUnit.Framework;
+
+    public class BayesianAnalysis
+    {
+
+        [Test]
+        public void TestScoreBasedLearning()
+        {
+            var model = new SimpleBayesianExampleModel();
+            Func<bool> hazard = () => model.Component.Hazard;
+            Func<bool> state = () => model.Component.NoDataAvailable || model.Component.SubsystemError;
+            var states = new Dictionary<string, Func<bool>> { /*["State"] = state*/ };
+            var bayesianCreator = new BayesianNetworkCreator(model, 10);
+
+            var config = BayesianNetworkCreator.Config;
+            config.UseDccaResultsForLearning = true;
+            config.UseRealProbabilitiesForSimulation = true;
+            BayesianNetworkCreator.Config = config;
+
+            var result = bayesianCreator.LearnScoreBasedBayesianNetwork(100000, hazard, states);
+        }
+
+        [Test]
+        public void TestConstraintBasedLearning()
+        {
+            var model = new SimpleBayesianExampleModel();
+            Func<bool> hazard = () => model.Component.Hazard;
+            Func<bool> state = () => model.Component.NoDataAvailable || model.Component.SubsystemError;
+            var states = new Dictionary<string, Func<bool>> { /*["State"] = state*/ };
+            var bayesianNetworkCreator = new BayesianNetworkCreator(model, 10);
+
+            var config = BayesianNetworkCreator.Config;
+            config.UseDccaResultsForLearning = true;
+            BayesianNetworkCreator.Config = config;
+
+            var result = bayesianNetworkCreator.LearnConstraintBasedBayesianNetwork(hazard, states, new[] { model.Component.FL, model.Component.FS, model.Component.FV });
+        }
+
+        [Test]
+        public void CalculateHazardProbability()
+        {
+            var tc = SafetySharpModelChecker.TraversalConfiguration;
+            tc.WriteGraphvizModels = true;
+            tc.MomentOfIndependentFaultActivation = MomentOfIndependentFaultActivation.OnFirstMethodWithoutUndo;
+            SafetySharpModelChecker.TraversalConfiguration = tc;
+
+            var model = new SimpleBayesianExampleModel();
+            var result = SafetySharpModelChecker.CalculateProbabilityToReachStateBounded(model, model.Component.Hazard, 20);
+
+            Console.WriteLine($"Probability of hazard in model: {result}");
+        }
+
+        [Test]
+        public void CalculateFaultProbabilities()
+        {
+            var tc = SafetySharpModelChecker.TraversalConfiguration;
+            tc.WriteGraphvizModels = true;
+            tc.MomentOfIndependentFaultActivation = MomentOfIndependentFaultActivation.OnFirstMethodWithoutUndo;
+            SafetySharpModelChecker.TraversalConfiguration = tc;
+
+            var model = new SimpleBayesianExampleModel();
+
+            var isFlActivated = SafetySharpModelChecker.CalculateProbabilityToReachStateBounded(model, model.Component.FL.IsActivated, 20);
+            var isFvActivated = SafetySharpModelChecker.CalculateProbabilityToReachStateBounded(model, model.Component.FV.IsActivated, 20);
+            var isFsActivated = SafetySharpModelChecker.CalculateProbabilityToReachStateBounded(model, model.Component.FS.IsActivated, 20);
+
+
+            Console.WriteLine($"Probability that Fault1 is activated: {isFlActivated}");
+            Console.WriteLine($"Probability that Fault2 is activated: {isFvActivated}");
+            Console.WriteLine($"Probability that Fault2 is activated: {isFsActivated}");
+        }
+
+        [Test]
+        public void CalculateDcca()
+        {
+            var model = new SimpleBayesianExampleModel();
+
+            var analysis = new SafetySharpSafetyAnalysis
+            {
+                Backend = SafetyAnalysisBackend.FaultOptimizedOnTheFly,
+                Heuristics = { new MaximalSafeSetHeuristic(model.Faults) }
+            };
+            var result = analysis.ComputeMinimalCriticalSets(model, model.Component.Hazard);
+
+            var orderResult = SafetySharpOrderAnalysis.ComputeOrderRelationships(result);
+            Console.WriteLine(orderResult);
+        }
+    }
+}
---
+++ Models/Small Models/SimpleBayesianExample/SimpleBayesianExample.cs
@@ -0,0 +1,99 @@
+﻿namespace SafetySharp.CaseStudies.SmallModels.SimpleBayesianExample
+{
+    using ISSE.SafetyChecking.Modeling;
+    using Modeling;
+
+    public class SimpleBayesianExampleComponent : Component
+    {
+        public SimpleBayesianExampleComponent()
+        {
+            FL = new TransientFault();
+            FL.ProbabilityOfOccurrence = new Probability(0.4);
+
+            FV = new TransientFault();
+            FV.ProbabilityOfOccurrence = new Probability(0.01);
+
+            FS = new TransientFault();
+            FS.ProbabilityOfOccurrence = new Probability(0.05);
+        }
+
+        public Fault FL, FV, FS;
+
+        public int Step;
+        public bool SubsystemError;
+        public bool SensorDefect;
+        public bool NoDataAvailable;
+        public bool Hazard => SubsystemError && SensorDefect;
+
+        public override void Update()
+        {
+            if (Step >= 3)
+                return;
+
+            if (Step == 0)
+                RequestData();
+            if (Step == 2 || NoDataAvailable)
+            {
+                CheckSensor();
+                AskSubsystem();
+            }
+
+            Step++;
+        }
+
+        public virtual void RequestData()
+        {
+            // Get Data from some oracle
+        }
+
+        public virtual void AskSubsystem()
+        {
+            // Request data from own subsystem
+            SubsystemError = false;
+        }
+
+        public virtual void CheckSensor()
+        {
+            // Measure data from own sensor
+        }
+
+
+        [FaultEffect(Fault = nameof(FL)), Priority(1)]
+        public class FLEffect : SimpleBayesianExampleComponent
+        {
+            public override void RequestData()
+            {
+                // Oracle data was flawed or not available
+                NoDataAvailable = true;
+            }
+        }
+
+        [FaultEffect(Fault = nameof(FV)), Priority(0)]
+        public class FVEffect : SimpleBayesianExampleComponent
+        {
+            public override void AskSubsystem()
+            {
+                // Data of the subsystem was flawed or not available
+                SubsystemError = true;
+            }
+        }
+
+        [FaultEffect(Fault = nameof(FS)), Priority(2)]
+        public class FSEffect : SimpleBayesianExampleComponent
+        {
+            public override void CheckSensor()
+            {
+                // Sensor breaks and is from now on defect
+                SensorDefect = true;
+            }
+        }
+    }
+
+
+    public sealed class SimpleBayesianExampleModel : ModelBase
+    {
+        [Root(RootKind.Controller)]
+        public SimpleBayesianExampleComponent Component { get; } = new SimpleBayesianExampleComponent();
+    }
+
+}
