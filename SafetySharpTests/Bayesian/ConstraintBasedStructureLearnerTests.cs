07529f8221781c3c1e17048863fa78901f1021bd
Stefan Fritsch <stef.fritsch@gmx.de>
2017-11-14 20:00:46 +0100

    Tests for bayesian analysis

---
+++ SafetySharpTests/Bayesian/ConstraintBasedStructureLearnerTests.cs
@@ -0,0 +1,184 @@
+﻿namespace Tests.Bayesian
+{
+    using System.Collections.Generic;
+    using SafetySharp.Bayesian;
+    using Xunit;
+
+    public class ConstraintBasedStructureLearnerTests
+    {
+
+        [Fact]
+        public void TestModel2()
+        {
+            var fl = new BooleanRandomVariable(() => true, "FL");
+            var fv = new BooleanRandomVariable(() => true, "FV");
+            var h = new BooleanRandomVariable(() => true, "H");
+            var independencies = new DualKeyDictionary<RandomVariable, ICollection<ISet<RandomVariable>>>
+            {
+                [h, fl] = new[] { new HashSet<RandomVariable> { fv } }
+            };
+            var learning = new ConstraintBasedStructureLearner(new[] { fl, fv, h }, independencies);
+            learning.LearnDag();
+        }
+
+        [Fact]
+        public void TestExample()
+        {
+            var x = new BooleanRandomVariable(() => true, "x");
+            var y = new BooleanRandomVariable(() => true, "y");
+            var z = new BooleanRandomVariable(() => true, "z");
+            var w = new BooleanRandomVariable(() => true, "w");
+            var t = new BooleanRandomVariable(() => true, "t");
+            var independencies = new DualKeyDictionary<RandomVariable, ICollection<ISet<RandomVariable>>>
+            {
+                [x, y] = new[] { new HashSet<RandomVariable>() }, // x to y
+                [z, w] = new[] { new HashSet<RandomVariable> { x, y } }, // z to w given x,y
+                [w, x] = new[] { new HashSet<RandomVariable> { y } }, // w to x given y
+                [w, z] = new[] { new HashSet<RandomVariable> { y } }, // w to z given y
+                [t, x] = new[] { new HashSet<RandomVariable> { z, w } }, // t to x given z,w
+                [t, y] = new[] { new HashSet<RandomVariable> { z, w } } //t to y given z,w
+            };
+            var learning = new ConstraintBasedStructureLearner(new[] { x, y, z, w, t }, independencies);
+            learning.LearnDag();
+        }
+
+        [Fact]
+        public void Example10_1()
+        {
+            var x = new BooleanRandomVariable(() => true, "x");
+            var y = new BooleanRandomVariable(() => true, "y");
+            var independencies = new DualKeyDictionary<RandomVariable, ICollection<ISet<RandomVariable>>>
+            {
+                [x, y] = new[] { new HashSet<RandomVariable>() } // X to Y
+            };
+            var learning = new ConstraintBasedStructureLearner(new[] { x, y }, independencies);
+            var dag = learning.LearnDag();
+
+            var expected = new[,]
+            {
+                { 0, 0 },
+                { 0, 0 }
+            };
+            Assert.True(dag.EqualsAdjacencyMatrix(expected));
+        }
+
+
+        [Fact]
+        public void Example10_2()
+        {
+            var x = new BooleanRandomVariable(() => true, "x");
+            var y = new BooleanRandomVariable(() => true, "y");
+            var independencies = new DualKeyDictionary<RandomVariable, ICollection<ISet<RandomVariable>>>();
+            var learning = new ConstraintBasedStructureLearner(new[] { x, y }, independencies);
+            var dag = learning.LearnDag();
+
+            var expected = new[,]
+            {
+                { 0, 1 },
+                { 1, 0 }
+            };
+            Assert.True(dag.EqualsAdjacencyMatrix(expected));
+        }
+
+
+        [Fact]
+        public void Example10_3()
+        {
+            var x = new BooleanRandomVariable(() => true, "x");
+            var y = new BooleanRandomVariable(() => true, "y");
+            var z = new BooleanRandomVariable(() => true, "z");
+            var independencies = new DualKeyDictionary<RandomVariable, ICollection<ISet<RandomVariable>>>
+            {
+                [x, y] = new[] { new HashSet<RandomVariable> { z } } // X to Y given Z
+            };
+            var learning = new ConstraintBasedStructureLearner(new[] { x, y, z }, independencies);
+            var dag = learning.LearnDag();
+
+            var expected = new[,]
+            {
+                { 0, 0, 1 },
+                { 0, 0, 1 },
+                { 1, 1, 0 }
+            };
+            Assert.True(dag.EqualsAdjacencyMatrix(expected));
+        }
+
+        [Fact]
+        public void Example10_4()
+        {
+            var x = new BooleanRandomVariable(() => true, "x");
+            var y = new BooleanRandomVariable(() => true, "y");
+            var z = new BooleanRandomVariable(() => true, "z");
+            var independencies = new DualKeyDictionary<RandomVariable, ICollection<ISet<RandomVariable>>>
+            {
+                [x, y] = new[] { new HashSet<RandomVariable>() } // X to Y
+            };
+            var learning = new ConstraintBasedStructureLearner(new[] { x, y, z }, independencies);
+            var dag = learning.LearnDag();
+
+            var expected = new[,]
+            {
+                { 0, 0, 1 },
+                { 0, 0, 1 },
+                { 0, 0, 0 }
+            };
+            Assert.True(dag.EqualsAdjacencyMatrix(expected));
+        }
+
+        [Fact]
+        public void Example10_5()
+        {
+            var x = new BooleanRandomVariable(() => true, "x");
+            var y = new BooleanRandomVariable(() => true, "y");
+            var z = new BooleanRandomVariable(() => true, "z");
+            var w = new BooleanRandomVariable(() => true, "w");
+            var independencies = new DualKeyDictionary<RandomVariable, ICollection<ISet<RandomVariable>>>
+            {
+                [x, y] = new[] { new HashSet<RandomVariable>() }, // X to Y
+                [x, w] = new[] { new HashSet<RandomVariable> { z } }, // X to W given Z
+                [y, w] = new[] { new HashSet<RandomVariable> { z } } // Y to W given Z
+            };
+            var learning = new ConstraintBasedStructureLearner(new[] { x, y, z, w }, independencies);
+            var dag = learning.LearnDag();
+
+            var expected = new[,]
+            {
+                { 0, 0, 1, 0 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 1 },
+                { 0, 0, 0, 0 }
+            };
+            Assert.True(dag.EqualsAdjacencyMatrix(expected));
+        }
+
+        [Fact]
+        public void Example10_6()
+        {
+            var x = new BooleanRandomVariable(() => true, "x");
+            var y = new BooleanRandomVariable(() => true, "y");
+            var z = new BooleanRandomVariable(() => true, "z");
+            var u = new BooleanRandomVariable(() => true, "u");
+            var w = new BooleanRandomVariable(() => true, "w");
+            var independencies = new DualKeyDictionary<RandomVariable, ICollection<ISet<RandomVariable>>>
+            {
+                [x, y] = new[] { new HashSet<RandomVariable> { u } }, // X to Y given U
+                [u, z] = new[] { new HashSet<RandomVariable> { x, y } }, // U to Z given X,Y
+                [u, w] = new[] { new HashSet<RandomVariable> { x, y }, new HashSet<RandomVariable> { z } }, // U to W given {X,Y} and given {Z}
+                [x, w] = new[] { new HashSet<RandomVariable> { z } }, // X to W given Z
+                [y, w] = new[] { new HashSet<RandomVariable> { z } }, // Y to W given Z
+            };
+            var learning = new ConstraintBasedStructureLearner(new[] { x, y, z, u, w }, independencies);
+            var dag = learning.LearnDag();
+
+            var expected = new[,]
+            {
+                { 0, 0, 1, 1, 0 },
+                { 0, 0, 1, 1, 0 },
+                { 0, 0, 0, 0, 1 },
+                { 1, 1, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 }
+            };
+            Assert.True(dag.EqualsAdjacencyMatrix(expected));
+        }
+    }
+}
---
+++ SafetySharpTests/Bayesian/DagPatternTests.cs
@@ -0,0 +1,196 @@
+﻿namespace Tests.Bayesian
+{
+    using System.Linq;
+    using SafetySharp.Bayesian;
+    using Xunit;
+
+    public class DagPatternTests
+    {
+
+        [Fact]
+        public void TestEmptyDag()
+        {
+            const string n1 = "N1", n2 = "N2", n3 = "N3", n4 = "N4";
+            var dag = DagPattern<string>.InitEmptyDag(new[] { n1, n2, n3, n4 });
+            var expectedMatrix = new[,]
+            {
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 }
+            };
+            Assert.True(dag.EqualsAdjacencyMatrix(expectedMatrix));
+        }
+
+        [Fact]
+        public void TestCompleteDag()
+        {
+            const string n1 = "N1", n2 = "N2", n3 = "N3", n4 = "N4";
+            var dag = DagPattern<string>.InitCompleteDag(new[] { n1, n2, n3, n4 });
+            var expectedMatrix = new[,]
+            {
+                { 0, 1, 1, 1 },
+                { 1, 0, 1, 1 },
+                { 1, 1, 0, 1 },
+                { 1, 1, 1, 0 }
+            };
+            Assert.True(dag.EqualsAdjacencyMatrix(expectedMatrix));
+        }
+
+        [Fact]
+        public void TestDagFromMatrix()
+        {
+            const string n1 = "N1", n2 = "N2", n3 = "N3", n4 = "N4";
+            var matrix = new[,]
+            {
+                { 0, 0, 1, 1 },
+                { 1, 0, 0, 1 },
+                { 1, 0, 0, 0 },
+                { 0, 1, 1, 0 }
+            };
+            var dag = DagPattern<string>.InitDagWithMatrix(new[] { n1, n2, n3, n4 }, matrix);
+            Assert.True(dag.EqualsAdjacencyMatrix(matrix));
+        }
+
+        [Fact]
+        public void TestRemoving()
+        {
+            const string n1 = "N1", n2 = "N2", n3 = "N3", n4 = "N4";
+            var matrix = new[,]
+            {
+                { 0, 0, 1, 1 },
+                { 1, 0, 0, 1 },
+                { 1, 0, 0, 0 },
+                { 0, 1, 1, 0 }
+            };
+            var dag = DagPattern<string>.InitDagWithMatrix(new[] { n1, n2, n3, n4 }, matrix);
+            var modifiedMatrix = (int[,])matrix.Clone();
+            dag.RemoveEdge(n1, n4);
+            modifiedMatrix[0, 3] = 0;
+            dag.RemoveEdge(n2, n1);
+            modifiedMatrix[1, 0] = 0;
+
+            Assert.True(dag.EqualsAdjacencyMatrix(modifiedMatrix));
+        }
+
+        [Fact]
+        public void TestOrienting()
+        {
+            const string n1 = "N1", n2 = "N2", n3 = "N3", n4 = "N4";
+            var matrix = new[,]
+            {
+                { 0, 0, 1, 1 },
+                { 1, 0, 0, 1 },
+                { 1, 0, 0, 0 },
+                { 0, 1, 1, 0 }
+            };
+            var dag = DagPattern<string>.InitDagWithMatrix(new[] { n1, n2, n3, n4 }, matrix);
+            var modifiedMatrix = (int[,])matrix.Clone();
+
+            dag.OrientUndirectedEdge(n2, n1); // n2 -> n1 is not directed
+            dag.OrientUndirectedEdge(n2, n3); // n2 -> n3 does not exist
+            dag.OrientUndirectedEdge(n1, n3); // n1 - n3 gets n1 -> n3
+            modifiedMatrix[2, 0] = 0;
+            dag.OrientUndirectedEdge(n4, n2); // n4 - n2 gets n4 -> n2
+            modifiedMatrix[1, 3] = 0;
+
+            Assert.True(dag.EqualsAdjacencyMatrix(modifiedMatrix));
+        }
+
+        [Fact]
+        public void TestNodeRelations()
+        {
+            const string n1 = "N1", n2 = "N2", n3 = "N3", n4 = "N4";
+            var matrix = new[,]
+               {
+                { 0, 0, 1, 1 },
+                { 1, 0, 0, 1 },
+                { 1, 0, 0, 1 },
+                { 0, 1, 1, 0 }
+            };
+            var dag = DagPattern<string>.InitDagWithMatrix(new[] { n1, n2, n3, n4 }, matrix);
+
+            var expectedChildren = new[] { n3, n4 };
+            var children = dag.GetChildren(n1);
+            Assert.True(!expectedChildren.Except(children).Any() && expectedChildren.Length == children.Count);
+
+            var expectedRealChildren = new[] { n4 };
+            var realChildren = dag.GetDirectedChildren(n1);
+            Assert.True(!expectedRealChildren.Except(realChildren).Any() && expectedRealChildren.Length == realChildren.Count);
+
+            var expectedParents = new[] { n2, n3 };
+            var parents = dag.GetParents(n1);
+            Assert.True(!expectedParents.Except(parents).Any() && expectedParents.Length == parents.Count);
+
+            var expectedRealParents = new[] { n2 };
+            var realParents = dag.GetDirectedParents(n1);
+            Assert.True(!expectedRealParents.Except(realParents).Any() && expectedRealParents.Length == realParents.Count);
+
+            var expectedUndirectedNeighbors = new[] { n1, n4 };
+            var undirectedNeighbors = dag.GetUndirectedNeighbors(n3);
+            Assert.True(!expectedUndirectedNeighbors.Except(undirectedNeighbors).Any() && expectedUndirectedNeighbors.Length == undirectedNeighbors.Count);
+        }
+
+        [Fact]
+        public void TestIsDagCycle()
+        {
+            const string n1 = "N1", n2 = "N2", n3 = "N3", n4 = "N4";
+            var matrix = new[,]
+            {
+                { 0, 1, 0, 0 },
+                { 0, 0, 1, 0 },
+                { 1, 0, 0, 1 },
+                { 0, 0, 0, 0 }
+            };
+            var dag = DagPattern<string>.InitDagWithMatrix(new[] { n1, n2, n3, n4 }, matrix);
+            Assert.False(dag.IsDag());
+        }
+
+        [Fact]
+        public void TestIsDagUndirectedEdge()
+        {
+            const string n1 = "N1", n2 = "N2", n3 = "N3", n4 = "N4";
+            var matrix = new[,]
+            {
+                { 0, 1, 0, 0 },
+                { 1, 0, 1, 0 },
+                { 0, 0, 0, 1 },
+                { 0, 0, 0, 0 }
+            };
+            var dag = DagPattern<string>.InitDagWithMatrix(new[] { n1, n2, n3, n4 }, matrix);
+            Assert.False(dag.IsDag());
+        }
+
+        [Fact]
+        public void TestIsDag()
+        {
+            const string n1 = "N1", n2 = "N2", n3 = "N3", n4 = "N4";
+            var matrix = new[,]
+            {
+                { 0, 1, 0, 1 },
+                { 0, 0, 1, 1 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 1, 0 }
+            };
+            var dag = DagPattern<string>.InitDagWithMatrix(new[] { n1, n2, n3, n4 }, matrix);
+            Assert.True(dag.IsDag());
+        }
+
+        [Fact]
+        public void TestReachability()
+        {
+            const string n1 = "N1", n2 = "N2", n3 = "N3", n4 = "N4";
+            var matrix = new[,]
+               {
+                { 0, 1, 1, 1 },
+                { 1, 0, 1, 0 },
+                { 1, 0, 0, 0 },
+                { 0, 1, 0, 0 }
+            };
+            var dag = DagPattern<string>.InitDagWithMatrix(new[] { n1, n2, n3, n4 }, matrix);
+
+            Assert.True(dag.IsReachableWithDirectedEdges(n1, n3));
+            Assert.False(dag.IsReachableWithDirectedEdges(n2, n1));
+        }
+    }
+}
