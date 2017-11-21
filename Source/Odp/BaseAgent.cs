2b67ad1ff63e13482e15ae5f8d51c1b364123b68
maul.esel <maul.esel@go4more.de>
2017-11-21 11:31:45 +0100

    fix role allocation check: was too strict
    Add comment, improve exception messages.

--- Source/Odp/BaseAgent.cs
+++ Source/Odp/BaseAgent.cs
@@ -553,10 +553,18 @@ namespace SafetySharp.Odp
 			{
 				Debug.Assert(role.IsValid);
 #if DEBUG
-				if (_allocatedRoles.Any(r => r.PreCondition.StateMatches(role.PreCondition)))
-					throw new Exception("Duplicate precondition");
-				if (_allocatedRoles.Any(r => r.PostCondition.StateMatches(role.PostCondition)))
-					throw new Exception("Duplicate postcondition");
+				// Check for duplicate conditions between roles. While not really a problem for ODP (use case: load distribution), current
+				// reconfiguration algorithms shouldn't produce such configurations. Hence this check to indicate potential problems.
+				// Previously only checked for conditions with the same state, instead of exactly the same. This check is currently too strict,
+				// since the coalition algorithm sometimes produces such configurations - although inefficient, they are still valid.
+
+				var index = _allocatedRoles.FindIndex(r => r.PreCondition == role.PreCondition);
+				if (index != -1)
+					throw new Exception($"Duplicate precondition at agent {Id}: pre-existing role {_allocatedRoles[index]} and new role {role}");
+
+				index = _allocatedRoles.FindIndex(r => r.PostCondition == role.PostCondition);
+				if (index != -1)
+					throw new Exception($"Duplicate postcondition at agent {Id}: pre-existing role {_allocatedRoles[index]} and new role {role}");
 #endif
 				_allocatedRoles.Add(role);
 			}
