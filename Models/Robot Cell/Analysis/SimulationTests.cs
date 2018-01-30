36ca3739b0f8e3c4c8dcf7059a3cb946dde5060f
maul.esel <maul.esel@go4more.de>
2018-01-30 15:30:09 +0100

    increase time limit

--- Models/Robot Cell/Analysis/SimulationTests.cs
+++ Models/Robot Cell/Analysis/SimulationTests.cs
@@ -1,17 +1,17 @@
 ﻿// The MIT License (MIT)
-// 
-// Copyright (c) 2014-2016, Institute for Software & Systems Engineering
-// 
+//
+// Copyright (c) 2014-2018, Institute for Software & Systems Engineering
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -89,7 +89,7 @@ namespace SafetySharp.CaseStudies.RobotCell.Analysis
 
 			const int simulationsPerModel = 100;
 			const int numberOfSteps = 1000;
-			const int timeLimitMs = 600000;
+			var timeLimit = TimeSpan.FromMinutes(20);
 
 			var reportsDirectory = Path.Combine("performance-reports", TestContext.CurrentContext.Test.Name);
 
@@ -117,7 +117,7 @@ namespace SafetySharp.CaseStudies.RobotCell.Analysis
 
 					console.WriteLine("\tSimulating...");
 					thread.Start();
-					var completed = thread.Join(timeLimitMs);
+					var completed = thread.Join(timeLimit);
 
 					if (!completed)
 					{
