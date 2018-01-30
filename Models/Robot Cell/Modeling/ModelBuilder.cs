e78f0c2eac10a3b5fafd4596ecc63b8da63d981c
maul.esel <maul.esel@go4more.de>
2018-01-30 16:39:02 +0100

    carts are now included in profile-based simulation

--- Models/Robot Cell/Modeling/ModelBuilder.cs
+++ Models/Robot Cell/Modeling/ModelBuilder.cs
@@ -1,17 +1,17 @@
 ﻿// The MIT License (MIT)
-// 
-// Copyright (c) 2014-2016, Institute for Software & Systems Engineering
-// 
+//
+// Copyright (c) 2014-2018, Institute for Software & Systems Engineering
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -122,7 +122,10 @@ namespace SafetySharp.CaseStudies.RobotCell.Modeling
 
 			var cart = _usePlants ? CreateCart(RouteHelper.ToRoutes(routeSpecifications, _model)) : null;
 			var agent = new CartAgent(cart) { TaskQueue = _model.TaskQueue };
+
 			_model.CartAgents.Add(agent);
+			_model.ReconfigurationMonitor.AddAgent(agent);
+			agent.ReconfigurationMonitor = _model.ReconfigurationMonitor;
 			cart?.SetNames(agent.Id);
 
 			RouteHelper.Connect(agent, routeSpecifications, _model);
