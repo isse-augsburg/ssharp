7cff8d104eb0d204b4bc6e986dff7f5d0c15416d
Pascal Pfeil <pfeilpascal@outlook.de>
2018-04-23 13:30:04 +0200

    Added Unit Tests for ANTLR

---
+++ Source/SafetyLustre.ANTLR.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SafetyLustre.ANTLR.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SafetyLustre.ANTLR.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1bf68d6-a34d-4892-9a5c-7db3a4f2a01d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
